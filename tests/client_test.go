5cac43e84277b831af6e4a7e39743266f9513550
michaeldcanady <michaeldcanady+github@gmail.com>
2023-08-13 14:57:04 -0400

    adding test to make sure it adjusts the url appropaitely

--- tests/client_test.go
+++ tests/client_test.go
@@ -15,3 +15,13 @@ func TestNewClient(t *testing.T) {
 
 	assert.NotNil(t, client)
 }
+
+func TestClientURL(t *testing.T) {
+
+	cred := servicenowsdkgo.NewUsernamePasswordCredential("username", "password")
+
+	client := servicenowsdkgo.NewClient(cred, "instance")
+
+	assert.Equal(t, client.BaseUrl, "instance.service-now.com")
+
+}
