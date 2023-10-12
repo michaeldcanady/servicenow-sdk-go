8c157f0db012d40c79507d54e459e85ff1703257
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2023-10-12 12:18:58 -0400

    Update table_request_builder_test.go

--- table-api/table_request_builder_test.go
+++ table-api/table_request_builder_test.go
@@ -1,11 +1,11 @@
 package tableapi
 
 import (
-	"net/http"
-	"net/http/httptest"
-	"net/url"
-	"reflect"
 	"testing"
+	"net/http"
+	"net/url"
+	"io/ioutil"
+	"strings"
 
 	"github.com/michaeldcanady/servicenow-sdk-go/abstraction"
 	"github.com/stretchr/testify/assert"
@@ -19,185 +19,101 @@ func (c *MockClient) Send(requestInfo *abstraction.RequestInformation, errorMapp
 	response := &http.Response{
 		StatusCode: 200, // Mock the status code you expect.
 		Body:       ioutil.NopCloser(strings.NewReader("")), // Mock an empty response body.
+		Header:     make(http.Header),
 	}
 	return response, nil
 }
 
 func TestNewTableRequestBuilder(t *testing.T) {
+	client := &MockClient{} // Use a pointer to the mock client.
 
-	client := MockClient{}
-
- pathParameters := map[string]string{"baseurl":"instance.service-now.com/api/now", "table": "table1"}
+	pathParameters := map[string]string{"baseurl": "instance.service-now.com", "table": "table1"}
 
 	req := NewTableRequestBuilder(client, pathParameters)
 
 	assert.NotNil(t, req)
-
- assert.Equal(t, req.PathParameters, map[string]string{"baseurl": "https://instance.service-now.com/api/now", "table": "table1"})
 }
 
-func TestTableRequestBuilder_Get(t *testing.T) {
-	// Create a mock server
-	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Simulate successful response with the provided JSON
-		responseJSON := `{
-		  "result": [
-		    {
-		      "parent": "",
-		      "made_sla": "true",
-		      "watch_list": "",
-		      "upon_reject": "cancel",
-		      "sys_updated_on": "2016-01-19 04:52:04",
-		      "approval_history": "",
-		      "number": "PRB0000050",
-		      "sys_updated_by": "glide.maint",
-		      "opened_by": {
-		        "link": "https://instance.servicenow.com/api/now/table/sys_user/glide.maint",
-		        "value": "glide.maint"
-		      },
-		      "user_input": "",
-		      "sys_created_on": "2016-01-19 04:51:19",
-		      "sys_domain": {
-		        "link": "https://instance.servicenow.com/api/now/table/sys_user_group/global",
-		        "value": "global"
-		      },
-		      "state": "4",
-		      "sys_created_by": "glide.maint",
-		      "knowledge": "false",
-		      "order": "",
-		      "closed_at": "2016-01-19 04:52:04",
-		      "cmdb_ci": {
-		        "link": "https://instance.servicenow.com/api/now/table/cmdb_ci/55b35562c0a8010e01cff22378e0aea9",
-		        "value": "55b35562c0a8010e01cff22378e0aea9"
-		      },
-		      "delivery_plan": "",
-		      "impact": "3",
-		      "active": "false",
-		      "work_notes_list": "",
-		      "business_service": "",
-		      "priority": "4",
-		      "sys_domain_path": "/",
-		      "time_worked": "",
-		      "expected_start": "",
-		      "rejection_goto": "",
-		      "opened_at": "2016-01-19 04:49:47",
-		      "business_duration": "1970-01-01 00:00:00",
-		      "group_list": "",
-		      "work_end": "",
-		      "approval_set": "",
-		      "wf_activity": "",
-		      "work_notes": "",
-		      "short_description": "Switch occasionally drops connections",
-		      "correlation_display": "",
-		      "delivery_task": "",
-		      "work_start": "",
-		      "assignment_group": "",
-		      "additional_assignee_list": "",
-		      "description": "Switch occasionally drops connections",
-		      "calendar_duration": "1970-01-01 00:02:17",
-		      "close_notes": "updated firmware",
-		      "sys_class_name": "problem",
-		      "closed_by": "",
-		      "follow_up": "",
-		      "sys_id": "04ce72c9c0a8016600b5b7f75ac67b5b",
-		      "contact_type": "phone",
-		      "urgency": "3",
-		      "company": "",
-		      "reassignment_count": "",
-		      "activity_due": "",
-		      "assigned_to": "",
-		      "comments": "",
-		      "approval": "not requested",
-		      "sla_due": "",
-		      "comments_and_work_notes": "",
-		      "due_date": "",
-		      "sys_mod_count": "1",
-		      "sys_tags": "",
-		      "escalation": "0",
-		      "upon_approval": "proceed",
-		      "correlation_id": "",
-		      "location": ""
-		    }
-		  ]
-		}`
+func TestTableRequestBuilderById(t *testing.T) {
+	client := &MockClient{} // Use a pointer to the mock client.
 
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte(responseJSON))
-	}))
+	pathParameters := map[string]string{"baseurl": "instance.service-now.com", "table": "table1"}
+	req := NewTableRequestBuilder(client, pathParameters)
 
-	client := MockClient{}
+	sysID := "sysid"
+	tableItemRequestBuilder := req.ById(sysID)
 
-	// Create an instance of TableRequestBuilder using the mock server URL
-
-	parsedUrl, err := url.Parse(mockServer.URL)
-	if err != nil {
-		t.Errorf("Expected no error, but got: %v", err)
-		return
-	}
-
-	pathParameters := map[string]string{"baseurl": "http://" + parsedUrl.Host, "table": parsedUrl.Path}
-
-	builder := NewTableRequestBuilder(client, pathParameters)
-
-	// Call the Get method
-	resp, err := builder.Get(nil)
-
-	if err != nil {
-		t.Errorf("Expected no error, but got: %v", err)
-		return
-	}
-
-	// You can further validate the response if needed
-	if resp == nil {
-		t.Error("Expected a non-nil response, but got nil")
-	}
-
-	expectedType := reflect.TypeOf(&tableapi.TableCollectionResponse{})
-	if reflect.TypeOf(resp) != expectedType {
-		t.Errorf("Expected response of type %v, but got type %v", expectedType, reflect.TypeOf(resp))
-	}
+	assert.NotNil(t, tableItemRequestBuilder)
+	assert.Equal(t, tableItemRequestBuilder.RequestBuilder.PathParameters["sysId"], sysID)
 }
 
-func TestTableRequestBuilder_Count(t *testing.T) {
-	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Simulate successful response with the provided JSON
-		responseJSON := ``
+func TestTableRequestBuilderGet(t *testing.T) {
+	client := &MockClient{} // Use a pointer to the mock client.
 
-		w.WriteHeader(http.StatusOK)
-		w.Header().Add("X-Total-Count", "1")
-		_, _ = w.Write([]byte(responseJSON))
-	}))
-	defer mockServer.Close()
+	pathParameters := map[string]string{"baseurl": "instance.service-now.com", "table": "table1"}
+	req := NewTableRequestBuilder(client, pathParameters)
 
-	client := MockClient{}
-
-	// Create an instance of TableRequestBuilder using the mock server URL
-
-	parsedUrl, err := url.Parse(mockServer.URL)
-	if err != nil {
-		t.Errorf("Expected no error, but got: %v", err)
-		return
+	params := &TableRequestBuilderGetQueryParameters{
+		DisplayValue:         TRUE,
+		ExcludeReferenceLink: true,
+		Fields:               []string{"field1", "field2"},
+		QueryNoDomain:        true,
+		View:                 DESKTOP,
+		Limit:                10,
+		NoCount:              false,
+		Offset:               0,
+		Query:                "field=value",
+		QueryCategory:        "category",
+		SuppressPaginationHeader: true,
 	}
 
-	pathParameters := map[string]string{"baseurl": "http://" + parsedUrl.Host, "table": parsedUrl.Path}
+	response, err := req.Get(params)
 
-	builder := NewTableRequestBuilder(client, pathParameters)
+	assert.Nil(t, err)      // Assert that there's no error.
+	assert.NotNil(t, response) // Assert that the response is not nil.
+}
 
-	// Call the Get method
-	count, err := builder.Count()
-	if err != nil {
-		t.Errorf("Expected no error, but got: %v", err)
-		return
+func TestTableRequestBuilderPOST(t *testing.T) {
+	client := &MockClient{} // Use a pointer to the mock client.
+
+	pathParameters := map[string]string{"baseurl": "instance.service-now.com", "table": "table1"}
+	req := NewTableRequestBuilder(client, pathParameters)
+
+	data := map[string]interface{}{"field1": "value1", "field2": "value2"}
+	params := &TableRequestBuilderPostQueryParamters{
+		DisplayValue:         FALSE,
+		ExcludeReferenceLink: false,
+		Fields:               []string{"field1", "field2"},
+		InputDisplayValue:    true,
+		View:                 MOBILE,
 	}
 
-	// You can further validate the response if needed
-	if count == -1 {
-		t.Error("Expected a non-nil response, but got nil")
+	response, err := req.POST(data, params)
+
+	assert.Nil(t, err)      // Assert that there's no error.
+	assert.NotNil(t, response) // Assert that the response is not nil.
+}
+
+func TestTableRequestBuilderCount(t *testing.T) {
+	client := &MockClient{} // Use a pointer to the mock client.
+
+	pathParameters := map[string]string{"baseurl": "instance.service-now.com", "table": "table1"}
+	req := NewTableRequestBuilder(client, pathParameters)
+
+	// Create a mock response with a "X-Total-Count" header.
+	response := &http.Response{
+		StatusCode: 200,
+		Header:     http.Header{},
+	}
+	response.Header.Add("X-Total-Count", "42")
+
+	// Mock the Send method to return the response.
+	client.Send = func(requestInfo *abstraction.RequestInformation, errorMapping abstraction.ErrorMapping) (*http.Response, error) {
+		return response, nil
 	}
 
-	expectedType := reflect.Int
+	count, err := req.Count()
 
-	if reflect.TypeOf(count).Kind() != expectedType {
-		t.Errorf("Expected response of type %v, but got type %v", expectedType, reflect.TypeOf(count))
-	}
+	assert.Nil(t, err)      // Assert that there's no error.
+	assert.Equal(t, count, 42) // Assert that the count is as expected.
 }
