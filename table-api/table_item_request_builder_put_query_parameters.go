57d5e4165750eff285fa898ec5b6a9492c473d5d
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2023-10-12 13:19:19 -0400

    Update table_item_request_builder_put_query_parameters.go

--- table-api/table_item_request_builder_put_query_parameters.go
+++ table-api/table_item_request_builder_put_query_parameters.go
@@ -1,2 +1,10 @@
 package tableapi
 
+type TableItemRequestBuilderPutQueryParameters struct {
+	DisplayValue         DisplayValue `uriparametername:"sysparm_display_value"`
+	ExcludeReferenceLink bool         `uriparametername:"sysparm_exclude_reference_link"`
+	Fields               []string     `uriparametername:"sysparm_fields"`
+	InputDisplayValue    bool         `uriparametername:"sysparm_input_display_value"`
+	QueryNoDomain        bool         `uriparametername:"sysparm_query_no_domain"`
+	View                 View         `uriparametername:"sysparm_view"`
+}
