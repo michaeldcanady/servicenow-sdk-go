5e9bbcae145538ba69a234df67bea13465d7a852
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2023-10-12 12:53:14 -0400

    Update display_value.go

--- table-api/display_value.go
+++ table-api/display_value.go
@@ -3,7 +3,7 @@ package tableapi
 type DisplayValue string
 
 const (
-    TRUE  DisplayValue = "true"
-    FALSE DisplayValue = "false"
+  TRUE  DisplayValue = "true"
+  FALSE DisplayValue = "false"
 	ALL   DisplayValue = "all"
 )
