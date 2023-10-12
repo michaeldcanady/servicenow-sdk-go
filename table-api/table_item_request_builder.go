b4d24e1731241daab22554c69867350c8e02c489
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2023-10-12 13:13:17 -0400

    Update table_item_request_builder.go

--- table-api/table_item_request_builder.go
+++ table-api/table_item_request_builder.go
@@ -6,17 +6,6 @@ type TableItemRequestBuilder struct {
 	abstraction.RequestBuilder
 }
 
-type TableItemRequestBuilderDeleteQueryParameters struct {
-	//Flag that indicates whether to restrict the record search to only the domains for which the logged in user is configured.
-	//
-	//Valid values:
-	//
-	//- false: Exclude the record if it is in a domain that the currently logged in user is not configured to access.
-	//
-	//- true: Include the record even if it is in a domain that the currently logged in user is not configured to access.
-	QueryNoDomain bool `uriparametername:"sysparm_query_no_domain"`
-}
-
 type TableItemRequestBuilderPutQueryParameters struct {
 	DisplayValue         DisplayValue `uriparametername:"sysparm_display_value"`
 	ExcludeReferenceLink bool         `uriparametername:"sysparm_exclude_reference_link"`
