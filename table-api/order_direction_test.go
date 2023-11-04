a4410f045981062e11c34f4e83520f3e93ac2a24
Michael Canady <michaeldcanady+github@gmail.com>
2023-11-03 22:21:36 -0400

    making queries functional

---
+++ table-api/order_direction_test.go
@@ -0,0 +1,23 @@
+package tableapi
+
+import "testing"
+
+func TestOrderDirections(t *testing.T) {
+	testCases := []struct {
+		direction OrderDirection
+		expected  string
+	}{
+		{Unset, ""},
+		{Asc, "^ORDERBY"},
+		{Desc, "^ORDERBYDESC"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(string(tc.direction), func(t *testing.T) {
+			result := string(tc.direction)
+			if result != tc.expected {
+				t.Errorf("Expected %s, got %s", tc.expected, result)
+			}
+		})
+	}
+}
