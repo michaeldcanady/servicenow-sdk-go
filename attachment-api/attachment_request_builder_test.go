137f5248cbcbdeb87d23138b601514bb199b25f5
Michael Canady <michaeldcanady+github@gmail.com>
2023-11-12 22:25:28 -0500

    fix: change no lint to all

--- attachment-api/attachment_request_builder_test.go
+++ attachment-api/attachment_request_builder_test.go
@@ -88,7 +88,7 @@ func TestAttachmentRequestBuilder_Get(t *testing.T) {
 		  }`
 
 		w.WriteHeader(http.StatusOK)
-		_, err := w.Write([]byte(responseJSON)) //nolint:errcheck
+		_, err := w.Write([]byte(responseJSON)) //nolint:all
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
