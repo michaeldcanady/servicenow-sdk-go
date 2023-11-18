404963c7f56f45cf059f6d1cd379e2b6c028c946
Michael Canady <michaeldcanady+github@gmail.com>
2023-11-17 21:52:32 -0500

    adding more tests

--- core/query_test.go
+++ core/query_test.go
@@ -1,6 +1,10 @@
 package core
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 type Test struct {
 	Title    string
@@ -8,7 +12,7 @@ type Test struct {
 	Actual   string
 }
 
-func TestAddQuery(t *testing.T) {
+func TestQuery_AddQuery(t *testing.T) {
 
 	tests := []Test{
 		{
@@ -37,12 +41,42 @@ func TestAddQuery(t *testing.T) {
 	}
 }
 
-func TestAddOrQuery(t *testing.T) {
+func TestQuery_AddOrQuery(t *testing.T) {
 	tests := []Test{
 		{
 			Title:    "Test AddOrQuery",
 			Expected: "field1=value1^ORfield2!=value2",
 			Actual:   NewQuery().AddOrQuery("field1", Is, "value1").AddOrQuery("field2", IsNot, "value2").String(),
+		},
+		{
+			Title:    "Test AddOrEqual",
+			Expected: "field1=value1^ORfield2=value2",
+			Actual:   NewQuery().AddOrEqual("field1", "value1").AddOrEqual("field2", "value2").String(),
+		},
+		{
+			Title:    "Test AddOrNotEqual",
+			Expected: "field1!=value1^ORfield2!=value2",
+			Actual:   NewQuery().AddOrNotEqual("field1", "value1").AddOrNotEqual("field2", "value2").String(),
+		},
+		{
+			Title:    "Test AddOrGreaterThan",
+			Expected: "field1>value1^ORfield2>value2",
+			Actual:   NewQuery().AddOrGreaterThan("field1", "value1").AddOrGreaterThan("field2", "value2").String(),
+		},
+		{
+			Title:    "Test AddOrLessThan",
+			Expected: "field1<value1^ORfield2<value2",
+			Actual:   NewQuery().AddOrLessThan("field1", "value1").AddOrLessThan("field2", "value2").String(),
+		},
+		{
+			Title:    "Test AddOrContains",
+			Expected: "field1CONTAINSvalue1^ORfield2CONTAINSvalue2",
+			Actual:   NewQuery().AddOrContains("field1", "value1").AddOrContains("field2", "value2").String(),
+		},
+		{
+			Title:    "Test AddOrNotContains",
+			Expected: "field1!CONTAINSvalue1^ORfield2!CONTAINSvalue2",
+			Actual:   NewQuery().AddOrNotContains("field1", "value1").AddOrNotContains("field2", "value2").String(),
 		},
 	}
 
@@ -55,7 +89,7 @@ func TestAddOrQuery(t *testing.T) {
 	}
 }
 
-func TestIsEmpty(t *testing.T) {
+func TestQuery_IsEmpty(t *testing.T) {
 	q := NewQuery()
 	q.IsEmpty("field3")
 	expected := "field3ISEMPTY"
@@ -64,7 +98,7 @@ func TestIsEmpty(t *testing.T) {
 	}
 }
 
-func TestOrderBy(t *testing.T) {
+func TestQuery_OrderBy(t *testing.T) {
 	q := NewQuery()
 	q.AddEqual("field1", true)
 	q.AddOrderBy("field4")
@@ -72,4 +106,19 @@ func TestOrderBy(t *testing.T) {
 	if result := q.String(); result != expected {
 		t.Errorf("Expected: %s, Got: %s", expected, result)
 	}
+
+	q = NewQuery()
+	q.AddEqual("field1", true)
+	q.AddOrderByDesc("field4")
+	expected = "field1=true^ORDERBYDESCfield4"
+	if result := q.String(); result != expected {
+		t.Errorf("Expected: %s, Got: %s", expected, result)
+	}
+}
+
+func TestQuery_String(t *testing.T) {
+	query := NewQuery()
+	actual := query.String()
+	expected := ""
+	assert.Equal(t, expected, actual)
 }
