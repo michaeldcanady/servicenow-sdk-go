2de7119fdc69fd0c5ab31891ec16c1aadaf24600
Michael Canady <michaeldcanady+github@gmail.com>
2023-12-09 21:26:44 -0500

    feature: adding tests

--- table-api/table_value_test.go
+++ table-api/table_value_test.go
@@ -6,6 +6,10 @@ import (
 	"testing"
 )
 
+func IsNotNilError(err error) bool {
+	return err != nil
+}
+
 func TestTableValueToInt64(t *testing.T) {
 	tests := []struct {
 		value      interface{}
@@ -40,8 +44,38 @@ func TestTableValueToInt64(t *testing.T) {
 	}
 }
 
-func IsNotNilError(err error) bool {
-	return err != nil
+func TestTableValueInt(t *testing.T) {
+	tests := []struct {
+		value      interface{}
+		expected   int64
+		expectErr  bool
+		errorCheck func(error) bool
+	}{
+		{int64(42), 42, false, nil},
+		{int64(123), 123, false, nil},
+		{float32(3.14), 0, true, IsNotNilError},
+		{"not an integer", 0, true, IsNotNilError},
+		{nil, 0, true, IsNotNilError},
+	}
+
+	for _, test := range tests {
+		t.Run("ToInt64", func(t *testing.T) {
+			tableValue := &TableValue{value: test.value}
+			result, err := tableValue.Int()
+
+			if test.errorCheck != nil {
+				if !test.errorCheck(err) {
+					t.Errorf("Expected error, got nil")
+				}
+			} else if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+
+			if result != test.expected {
+				t.Errorf("Expected %d, got %d", test.expected, result)
+			}
+		})
+	}
 }
 
 func TestTableValueToFloat64(t *testing.T) {
@@ -62,6 +96,40 @@ func TestTableValueToFloat64(t *testing.T) {
 		t.Run("ToFloat64", func(t *testing.T) {
 			tableValue := &TableValue{value: test.value}
 			result, err := tableValue.ToFloat64()
+
+			if test.errorCheck != nil {
+				if !test.errorCheck(err) {
+					t.Errorf("Expected error, got nil")
+				}
+			} else if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+
+			if result != test.expected {
+				t.Errorf("Expected %f, got %f", test.expected, result)
+			}
+		})
+	}
+}
+
+func TestTableValueFloat(t *testing.T) {
+	tests := []struct {
+		value      interface{}
+		expected   float64
+		expectErr  bool
+		errorCheck func(error) bool
+	}{
+		{float32(3.14), 0, true, IsNotNilError},
+		{float64(2.71828), 2.71828, false, nil},
+		{int(42), 0, true, IsNotNilError},
+		{"not a float", 0, true, IsNotNilError},
+		{nil, 0, true, IsNotNilError},
+	}
+
+	for _, test := range tests {
+		t.Run("ToFloat64", func(t *testing.T) {
+			tableValue := &TableValue{value: test.value}
+			result, err := tableValue.Float()
 
 			if test.errorCheck != nil {
 				if !test.errorCheck(err) {
@@ -111,6 +179,39 @@ func TestTableValueToString(t *testing.T) {
 	}
 }
 
+func TestTableValueString(t *testing.T) {
+	tests := []struct {
+		value      interface{}
+		expected   string
+		expectErr  bool
+		errorCheck func(error) bool
+	}{
+		{"hello", "hello", false, nil},
+		{"world", "world", false, nil},
+		{42, "", true, IsNotNilError},
+		{nil, "", true, IsNotNilError},
+	}
+
+	for _, test := range tests {
+		t.Run("ToString", func(t *testing.T) {
+			tableValue := &TableValue{value: test.value}
+			result, err := tableValue.String()
+
+			if test.errorCheck != nil {
+				if !test.errorCheck(err) {
+					t.Errorf("Expected error, got nil")
+				}
+			} else if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+
+			if result != test.expected {
+				t.Errorf("Expected %s, got %s", test.expected, result)
+			}
+		})
+	}
+}
+
 func TestTableValueGetType(t *testing.T) {
 	tests := []struct {
 		value    interface{}
@@ -127,6 +228,30 @@ func TestTableValueGetType(t *testing.T) {
 		t.Run(fmt.Sprintf("Value=%v", test.value), func(t *testing.T) {
 			tableValue := &TableValue{value: test.value}
 			result := tableValue.GetType()
+
+			if result != test.expected {
+				t.Errorf("Expected type %v, got %v", test.expected, result)
+			}
+		})
+	}
+}
+
+func TestTableValueType(t *testing.T) {
+	tests := []struct {
+		value    interface{}
+		expected reflect.Type
+	}{
+		{int(42), reflect.TypeOf(int(0))},
+		{float64(3.14), reflect.TypeOf(float64(0))},
+		{"hello", reflect.TypeOf("")},
+		{true, reflect.TypeOf(true)},
+		{nil, nil}, // Expected type for nil value
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("Value=%v", test.value), func(t *testing.T) {
+			tableValue := &TableValue{value: test.value}
+			result := tableValue.Type()
 
 			if result != test.expected {
 				t.Errorf("Expected type %v, got %v", test.expected, result)
