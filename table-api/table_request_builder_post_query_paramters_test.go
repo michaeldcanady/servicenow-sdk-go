58d2741b694f4550fbb852ea6ff7c77c34b7240c
Michael Canady <michaeldcanady+github@gmail.com>
2023-12-16 16:28:40 -0500

    fixing bad tests

--- table-api/table_request_builder_post_query_paramters_test.go
+++ table-api/table_request_builder_post_query_paramters_test.go
@@ -9,17 +9,17 @@ import (
 func TestTableRequestBuilderPostQueryParametersDefaults(t *testing.T) {
 	params := &TableRequestBuilderPostQueryParameters{}
 
-	assert.Equal(t, "", params.DisplayValue)
+	assert.Equal(t, DisplayValue(""), params.DisplayValue)
 	assert.Equal(t, false, params.ExcludeReferenceLink)
 	assert.Equal(t, 0, len(params.Fields))
-	assert.Equal(t, "", params.View)
+	assert.Equal(t, View(""), params.View)
 }
 
 func TestTableRequestBuilderPostQueryParamtersDefaults(t *testing.T) {
 	params := &TableRequestBuilderPostQueryParamters{}
 
-	assert.Equal(t, "", params.DisplayValue)
+	assert.Equal(t, DisplayValue(""), params.DisplayValue)
 	assert.Equal(t, false, params.ExcludeReferenceLink)
 	assert.Equal(t, 0, len(params.Fields))
-	assert.Equal(t, "", params.View)
+	assert.Equal(t, View(""), params.View)
 }
