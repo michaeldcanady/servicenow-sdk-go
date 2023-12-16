c48f17bb722b89943faa41ab51a1ebe87421f898
Michael Canady <michaeldcanady+github@gmail.com>
2023-12-16 15:26:17 -0500

    removing deprecated method

---
+++ attachment-api/attachment_collection_file_request_configuration.go
@@ -0,0 +1,21 @@
+package attachmentapi
+
+import "github.com/michaeldcanady/servicenow-sdk-go/core"
+
+type AttachmentCollectionFileRequestConfiguration struct {
+	Header          interface{}
+	QueryParameters *AttachmentRequestBuilderFileQueryParameters
+	Data            interface{}
+	ErrorMapping    core.ErrorMapping
+	response        *AttachmentItemResponse
+}
+
+func (rC *AttachmentCollectionFileRequestConfiguration) toConfiguration() *core.RequestConfiguration {
+	return &core.RequestConfiguration{
+		Header:          rC.Header,
+		QueryParameters: rC.QueryParameters,
+		Data:            rC.Data,
+		ErrorMapping:    rC.ErrorMapping,
+		Response:        rC.response,
+	}
+}
