b17af359cb1c8b778d574d51532e9209d18bd411
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2023-12-24 12:46:22 -0500

    Update table_request_builder2.go

--- table-api/table_request_builder2.go
+++ table-api/table_request_builder2.go
@@ -4,6 +4,19 @@ type TableRequestBuilder2[T TableEntry2] struct {
   core.RequestBuilder
 }
 
+// NewTableRequestBuilder creates a new instance of the TableRequestBuilder associated with the given URL and Client.
+// It accepts the URL and Client as parameters and returns a pointer to the created TableRequestBuilder.
+func NewTableRequestBuilder[T TableEntry2](client core.Client, pathParameters map[string]string) *TableRequestBuilder[T] {
+	requestBuilder := core.NewRequestBuilder(
+		client,
+		"{+baseurl}/table{/table}{?sysparm_display_value,sysparm_exclude_reference_link,sysparm_fields,sysparm_query_no_domain,sysparm_view,sysparm_limit,sysparm_no_count,sysparm_offset,sysparm_query,sysparm_query_category,sysparm_suppress_pagination_header}",
+		pathParameters,
+	)
+	return &TableRequestBuilder[T]{
+		*requestBuilder,
+	}
+}
+
 // Get sends an HTTP GET request using the specified query parameters and returns a TableCollectionResponse.
 //
 // Parameters:
