fa4a1e50965340088a2c1d4c46c88f0c01870679
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2023-12-24 12:43:32 -0500

    Update table_request_builder2.go

--- table-api/table_request_builder2.go
+++ table-api/table_request_builder2.go
@@ -3,3 +3,28 @@ package tableapi
 type TableRequestBuilder2[T TableEntry2] struct {
   core.RequestBuilder
 }
+
+// Get sends an HTTP GET request using the specified query parameters and returns a TableCollectionResponse.
+//
+// Parameters:
+//   - params: An instance of TableRequestBuilderGetQueryParameters to include in the GET request.
+//
+// Returns:
+//   - *TableCollectionResponse: The response data as a TableCollectionResponse.
+//   - error: An error if there was an issue with the request or response.
+func (rB *TableRequestBuilder2[T]) Get(params *TableRequestBuilderGetQueryParameters) (*TableCollectionResponse2[T], error) {
+	config := &TableGetRequestConfiguration2{
+		Header:          nil,
+		QueryParameters: params,
+		Data:            nil,
+		ErrorMapping:    nil,
+		response:        &TableCollectionResponse[T]{},
+	}
+
+	err := rB.SendGet2(config.toConfiguration())
+	if err != nil {
+		return nil, err
+	}
+
+	return config.response, nil
+}
