2690657769f0dfc386a25744324b360c9bccb9ee
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2023-12-24 12:35:24 -0500

    Update table_collection_response2.go

--- table-api/table_collection_response2.go
+++ table-api/table_collection_response2.go
@@ -1,1 +1,52 @@
 package tableapi
+
+import (
+	"net/http"
+	"regexp"
+)
+
+type TableCollectionResponse2[T TableEntry2] struct {
+	Result           []T
+	NextPageLink     string
+	PreviousPageLink string
+	FirstPageLink    string
+	LastPageLink     string
+}
+
+func (cR *TableCollectionResponse2[T]) parsePaginationHeaders(headers http.Header) {
+	linkHeaderRegex := regexp.MustCompile(`<([^>]+)>;rel="([^"]+)"`)
+
+	links := make(map[string]string)
+
+	hearderLinks := headers["Link"]
+
+	for _, header := range hearderLinks {
+		linkMatches := linkHeaderRegex.FindAllStringSubmatch(header, -1)
+
+		for _, match := range linkMatches {
+			link := match[1]
+			rel := match[2]
+
+			// Determine the type of link based on the 'rel' attribute
+			switch rel {
+			case "first":
+				links["firstPageLink"] = link
+			case "prev":
+				links["previousPageLink"] = link
+			case "next":
+				links["nextPageLink"] = link
+			case "last":
+				links["lastPageLink"] = link
+			}
+		}
+	}
+
+	cR.FirstPageLink = links["firstPageLink"]
+	cR.PreviousPageLink = links["previousPageLink"]
+	cR.NextPageLink = links["nextPageLink"]
+	cR.LastPageLink = links["lastPageLink"]
+}
+
+func (cR *TableCollectionResponse2[T]) ParseHeaders(headers http.Header) {
+	cR.parsePaginationHeaders(headers)
+}
