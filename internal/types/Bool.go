26dc5cb5f9bcc4aaacb4ecfed4067628f508137e
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2023-12-30 22:52:18 -0500

    Update Bool.go

---
+++ internal/types/Bool.go
@@ -0,0 +1,18 @@
+package types
+
+import (
+	"strconv"
+	"strings"
+)
+
+type Bool bool
+
+func (i *Bool) UnmarshalJSON(data []byte) error {
+	cleanData := strings.Replace(string(data), "\"", "", -1)
+
+	cleanInt, err := strconv.ParseBool(cleanData)
+
+	*i = Bool(cleanInt)
+
+	return err
+}
