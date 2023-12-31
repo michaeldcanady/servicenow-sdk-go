9799f2f8006dbc61fc5bcb9b317ef08f64c74ec6
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2023-12-30 22:53:33 -0500

    Update Int.go

---
+++ internal/types/Int.go
@@ -0,0 +1,22 @@
+package types
+
+import (
+	"strconv"
+	"strings"
+)
+
+type Int int
+
+func (i *Int) UnmarshalJSON(data []byte) error {
+	cleanData := strings.Replace(string(data), "\"", "", -1)
+
+	if cleanData == "" {
+		cleanData = "0"
+	}
+
+	cleanInt, err := strconv.Atoi(cleanData)
+
+	*i = Int(cleanInt)
+
+	return err
+}
