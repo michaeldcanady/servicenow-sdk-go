14f1e9b00730db210f042b7a6acea844d96cd612
michaeldcanady <michaeldcanady+github@gmail.com>
2024-02-12 21:09:41 -0500

    implement batch api

--- batch-api/batch_request_item.go
+++ batch-api/batch_request_item.go
@@ -8,7 +8,7 @@ import (
 	"github.com/michaeldcanady/servicenow-sdk-go/internal"
 )
 
-type BatchItem interface {
+type BatchRequestItem interface {
 	GetID() *string
 	SetID(value *string)
 	GetMethod() *string
@@ -23,7 +23,7 @@ type BatchItem interface {
 	SetExcludeResponseHeaders(bool)
 }
 
-type batchItem struct {
+type batchRequestItem struct {
 	ID                     *string       `json:"id"`
 	Method                 *string       `json:"method"`
 	ExcludeResponseHeaders bool          `json:"exclude_response_headers"`
@@ -33,45 +33,45 @@ type batchItem struct {
 }
 
 // NewBatchItem creates an instance of BatchItem
-func NewBatchItem(excludeResponseHeaders bool) BatchItem {
-	return &batchItem{
+func NewBatchItem(excludeResponseHeaders bool) BatchRequestItem {
+	return &batchRequestItem{
 		ExcludeResponseHeaders: excludeResponseHeaders,
 		Headers:                make([]batchHeader, 0),
 	}
 }
 
 // GetId returns batch item `id` property
-func (bi *batchItem) GetID() *string {
+func (bi *batchRequestItem) GetID() *string {
 	return bi.ID
 }
 
 // SetID sets string value as batch item `id` property
-func (bi *batchItem) SetID(value *string) {
+func (bi *batchRequestItem) SetID(value *string) {
 	bi.ID = value
 }
 
 // GetMethod returns batch item `Method` property
-func (bi *batchItem) GetMethod() *string {
+func (bi *batchRequestItem) GetMethod() *string {
 	return bi.Method
 }
 
 // SetMethod sets string value as batch item `Method` property
-func (bi *batchItem) SetMethod(value *string) {
+func (bi *batchRequestItem) SetMethod(value *string) {
 	bi.Method = value
 }
 
 // GetURL returns batch item `Url` property
-func (bi *batchItem) GetURL() *string {
+func (bi *batchRequestItem) GetURL() *string {
 	return bi.URL
 }
 
 // SetURL sets string value as batch item `Url` property
-func (bi *batchItem) SetURL(value *string) {
+func (bi *batchRequestItem) SetURL(value *string) {
 	bi.URL = value
 }
 
 // GetHeaders returns batch item `Header` as a map[string]string
-func (bi *batchItem) GetHeaders() internal.RequestHeader {
+func (bi *batchRequestItem) GetHeaders() internal.RequestHeader {
 	headers := internal.NewRequestHeader()
 
 	for _, header := range bi.Headers {
@@ -82,7 +82,7 @@ func (bi *batchItem) GetHeaders() internal.RequestHeader {
 }
 
 // SetHeaders sets map[string]string value as batch item `Header` property
-func (bi *batchItem) SetHeaders(value internal.RequestHeader) {
+func (bi *batchRequestItem) SetHeaders(value internal.RequestHeader) {
 	if value.Get(internal.ContentTypeHeader) == "" {
 		value.Set(internal.ContentTypeHeader, internal.JSONContentType)
 	}
@@ -105,7 +105,7 @@ func (bi *batchItem) SetHeaders(value internal.RequestHeader) {
 }
 
 // GetBody returns batch item `RequestBody` property
-func (bi *batchItem) GetBody() internal.RequestBody {
+func (bi *batchRequestItem) GetBody() internal.RequestBody {
 	var ret internal.RequestBody
 
 	data, _ := base64.StdEncoding.DecodeString(bi.Body)
@@ -116,18 +116,18 @@ func (bi *batchItem) GetBody() internal.RequestBody {
 }
 
 // SetBody sets map[string]string value as batch item `RequestBody` property
-func (bi *batchItem) SetBody(value internal.RequestBody) {
+func (bi *batchRequestItem) SetBody(value internal.RequestBody) {
 	data, _ := json.Marshal(value)
 
 	bi.Body = base64.StdEncoding.EncodeToString(data)
 }
 
 // GetExcludeResponseHeaders returns if batch item wasn't response headers excluded
-func (bi *batchItem) GetExcludeResponseHeaders() bool {
+func (bi *batchRequestItem) GetExcludeResponseHeaders() bool {
 	return bi.ExcludeResponseHeaders
 }
 
 // GetExcludeResponseHeaders sets if to exclude batch item response headers
-func (bi *batchItem) SetExcludeResponseHeaders(excludeResponseHeaders bool) {
+func (bi *batchRequestItem) SetExcludeResponseHeaders(excludeResponseHeaders bool) {
 	bi.ExcludeResponseHeaders = excludeResponseHeaders
 }
