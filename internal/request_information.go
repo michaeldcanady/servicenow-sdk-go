cab1e09d9cf6d80093706893a6560ea80c2606df
michaeldcanady <michaeldcanady+github@gmail.com>
2024-02-12 21:36:03 -0500

    correcting tests

--- internal/request_information.go
+++ internal/request_information.go
@@ -18,31 +18,3 @@ type RequestInformation interface {
 	AddHeaders(rawHeaders interface{}) error
 	GetHeaders() RequestHeader
 }
-
-type requestInformation struct {
-}
-
-func (rI *requestInformation) SetStreamContent(content []byte) {
-
-}
-func (rI *requestInformation) AddQueryParameters(source interface{}) error {
-	return nil
-}
-func (rI *requestInformation) SetUri(url *url.URL) {
-
-}
-func (rI *requestInformation) Url() (string, error) {
-	return "", nil
-}
-func (rI *requestInformation) ToRequest() (*http.Request, error) {
-	return rI.ToRequestWithContext(context.Background())
-}
-func (rI *requestInformation) ToRequestWithContext(ctx context.Context) (*http.Request, error) {
-	return nil, nil
-}
-func (rI *requestInformation) AddHeaders(rawHeaders interface{}) error {
-	return nil
-}
-func (rI *requestInformation) GetHeaders() RequestHeader {
-	return NewRequestHeader()
-}
