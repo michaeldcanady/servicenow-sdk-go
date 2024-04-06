60e43d78de6bccf516a8fa459c21c99a0d79dacf
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2024-04-05 23:38:29 -0400

    Update table_item_request_builder2.go

--- table-api/table_item_request_builder2.go
+++ table-api/table_item_request_builder2.go
@@ -1,6 +1,7 @@
 package tableapi
 
 type requestBuilder interface {
+    SendGet2(core.RequestConfigurations) error
 }
 
 type TableItemRequestBuilder2 struct {
@@ -17,4 +18,26 @@ func NewTableItemRequestBuilder2(client core.Client, pathParameters map[string]s
    }
 }
 
+// Get sends an HTTP GET request using the specified query parameters and returns a TableItemResponse.
+//
+// Parameters:
+//   - params: An instance of TableItemRequestBuilderGetQueryParameters to include in the GET request.
+//
+// Returns:
+//   - *TableItemResponse: The response data as a TableItemResponse.
+//   - error: An error if there was an issue with the request or response.
+func (rB *TableItemRequestBuilder2) Get(params *TableItemRequestBuilderGetQueryParameters) (*TableItemResponse, error) {
+	config := &tableItemGetRequestConfiguration2[TableEntry]{
+		header:   nil,
+		query:    params,
+		data:     nil,
+		response: &TableItemResponse2[TableEntry]{},
+	}
 
+	err := rB.SendGet2(config.toConfiguration()) //nolint:staticcheck
+	if err != nil {
+		return nil, err
+	}
+
+	return config.response, nil
+}
