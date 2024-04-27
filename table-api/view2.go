57b35fb5ae4f2d173854272113cf178cec3f8029
michaeldcanady <michaeldcanady+github@gmail.com>
2024-04-27 12:34:35 -0400

    goimports

--- table-api/view2.go
+++ table-api/view2.go
@@ -3,21 +3,21 @@ package tableapi
 type View2 int64
 
 const (
-    ViewDesktop View2 = iota
-    ViewMobile
-    ViewBoth
+	ViewDesktop View2 = iota
+	ViewMobile
+	ViewBoth
 )
 
 func (v View2) String() string {
-    views := map[View2]string{
-        ViewDesktop: "desktop",
-        ViewMobile: "mobile",
-        ViewBoth: "both",
-    }
+	views := map[View2]string{
+		ViewDesktop: "desktop",
+		ViewMobile:  "mobile",
+		ViewBoth:    "both",
+	}
 
-    str, ok := views[v]
-    if !ok {
-        return "invalid"
-    }
-    return str
+	str, ok := views[v]
+	if !ok {
+		return "invalid"
+	}
+	return str
 }
