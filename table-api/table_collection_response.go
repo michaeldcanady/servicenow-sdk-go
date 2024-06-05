9754788905ec046ac29c0ed6c1d0b676c0cf50d2
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2024-06-04 22:38:09 -0400

    Delete table-api/table_collection_response.go

--- table-api/table_collection_response.go
+++
@@ -1,6 +0,0 @@
-package tableapi
-
-// Deprecacted: deprecated since v1.4.0. Use "TableCollectionResponse2[T]" instead.
-//
-// TableCollectionResponse represents a collection of table entries.
-type TableCollectionResponse = TableCollectionResponse2[TableEntry]
