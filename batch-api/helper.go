2fe2906b215d493c81e8cfe3ef3dab4e2f65f725
Michael Canady <michaeldcanady+github@gmail.com>
2024-08-09 18:30:18 -0400

    missing tests

--- batch-api/helper.go
+++ batch-api/helper.go
@@ -2,10 +2,12 @@ package batchapi
 
 import (
 	"net/url"
-
-	"github.com/michaeldcanady/servicenow-sdk-go/core"
 )
 
-func getBaseURL(adapter core.Client2) (*url.URL, error) {
+type hasBaseURL interface {
+	GetBaseURL() string
+}
+
+func getBaseURL(adapter hasBaseURL) (*url.URL, error) {
 	return url.Parse(adapter.GetBaseURL())
 }
