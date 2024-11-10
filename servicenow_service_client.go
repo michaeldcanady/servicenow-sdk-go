5028a77ef7afc5ecf8217e262f3eb3193d6e0bb9
michaeldcanady <michaeldcanady+github@gmail.com>
2024-11-10 17:49:07 -0500

    bridging gap between old and new sdk

--- servicenow_service_client.go
+++ servicenow_service_client.go
@@ -2,24 +2,58 @@ package servicenowsdkgo
 
 import (
 	"context"
-	"net/url"
+	"errors"
 
+	"github.com/michaeldcanady/servicenow-sdk-go/core"
+	abstractions "github.com/microsoft/kiota-abstractions-go"
 	"github.com/microsoft/kiota-abstractions-go/authentication"
 	"github.com/microsoft/kiota-abstractions-go/store"
 )
+
+const authorizationHeader = "Authorization"
 
 type ServiceNowServiceClient struct {
 	ServiceNowBaseServiceClient
 }
 
 // TODO: will be new base for auth provider (credentials)
-type serviceNowAuthorizationProvider interface {
-	GetAuthorization(context.Context, *url.URL, map[string]interface{}) (string, error)
+type credentialAuthorizationProviderAdapter struct {
+	cred core.Credential
+}
+
+func (provider *credentialAuthorizationProviderAdapter) AuthenticateRequest(ctx context.Context, request *abstractions.RequestInformation, _ map[string]interface{}) error {
+
+	if request == nil {
+		return errors.New("request is nil")
+	}
+	if request.Headers == nil {
+		request.Headers = abstractions.NewRequestHeaders()
+	}
+	if provider.cred == nil {
+		return errors.New("this class needs to be initialized with a credential")
+	}
+	if !request.Headers.ContainsKey(authorizationHeader) {
+		authString, err := provider.cred.GetAuthentication()
+		if err != nil {
+			return err
+		}
+		if authString != "" {
+			request.Headers.Add(authorizationHeader, authString)
+		}
+	}
+
+	return nil
+}
+
+func NewServiceNowServiceClient2WithCredential(credential core.Credential, opts ...serviceNowServiceClientOption) (*ServiceNowServiceClient, error) {
+	authenticationProvider := &credentialAuthorizationProviderAdapter{
+		cred: credential,
+	}
+
+	return NewServiceNowServiceClient2(authenticationProvider, opts...)
 }
 
 func NewServiceNowServiceClient2(authenticationProvider authentication.AuthenticationProvider, opts ...serviceNowServiceClientOption) (*ServiceNowServiceClient, error) {
-
-	// TODO: make credential to request adapter conversion
 	requestAdapter, err := NewServiceNowRequestAdapterBase(authenticationProvider)
 	if err != nil {
 		return nil, err
