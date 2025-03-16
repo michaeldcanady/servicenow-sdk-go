6ab6f9df2c2113bf9cca3216ce2cd85cb16c5dd0
Michael Canady <michaeldcanady+github@gmail.com>
2025-03-16 03:22:07 +0000

    continuing to cover new additions

--- internal/new/kiota_request_information_options_test.go
+++ internal/new/kiota_request_information_options_test.go
@@ -1,5 +1,31 @@
 package internal
 
+import "testing"
+
+// TODO: add tests
+func TestWithMethod(t *testing.T) {
+	tests := []struct {
+		name string
+		test func(*testing.T)
+	}{}
+
+	for _, test := range tests {
+		t.Run(test.name, test.test)
+	}
+}
+
+// TODO: add tests
+func TestWithURLTemplate(t *testing.T) {
+	tests := []struct {
+		name string
+		test func(*testing.T)
+	}{}
+
+	for _, test := range tests {
+		t.Run(test.name, test.test)
+	}
+}
+
 import (
 	"errors"
 	"testing"
