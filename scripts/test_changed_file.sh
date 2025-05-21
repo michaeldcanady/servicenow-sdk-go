260349ee4477a0dc999fe365561122509e0344c6
Michael Canady <michaeldcanady+github@gmail.com>
2025-05-21 01:48:43 +0000

    fatal: ambiguous argument 'v1.7.0-preview20250518': unknown revision or path not in the working tree.

--- scripts/test_changed_file.sh
+++ scripts/test_changed_file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 LATEST_RELEASE_TAG=$(gh release list --limit 1 --json tagName --jq '.[0].tagName')
-echo $(git diff --name-only $LATEST_RELEASE_TAG HEAD | grep '\.go$' || true)
+echo $(git diff --name-only "${LATEST_RELEASE_TAG}" HEAD | grep '\.go$' || true)
--- scripts/test_commit.sh
+++ scripts/test_commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LATEST_RELEASE_TAG=$(gh release list --limit 1 --json tagName --jq '.[0].tagName')
-LATEST_RELEASE_COMMIT_ID=$(git rev-parse $LATEST_RELEASE_TAG)
+LATEST_RELEASE_COMMIT_ID=$(git rev-parse "${LATEST_RELEASE_TAG}")
 CURRENT_COMMIT_ID=$(git rev-parse HEAD)
 
 echo $($CURRENT_COMMIT_ID != $LATEST_RELEASE_COMMIT_ID)
