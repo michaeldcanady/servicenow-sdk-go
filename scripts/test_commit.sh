9550bbc485c3f2185e69a77d4d262b13fafebe33
Michael Canady <michaeldcanady+github@gmail.com>
2025-05-21 01:56:30 +0000

    attempting to get boolean response

--- scripts/test_commit.sh
+++ scripts/test_commit.sh
@@ -4,4 +4,8 @@ LATEST_RELEASE_TAG=$(gh release list --limit 1 --json tagName --jq '.[0].tagName
 LATEST_RELEASE_COMMIT_ID=$(git rev-parse "${LATEST_RELEASE_TAG}")
 CURRENT_COMMIT_ID=$(git rev-parse HEAD)
 
-echo $(["$CURRENT_COMMIT_ID" != "$LATEST_RELEASE_COMMIT_ID"])
+if ["$CURRENT_COMMIT_ID" != "$LATEST_RELEASE_COMMIT_ID"]; then
+    echo true
+else
+    echo false
+fi
