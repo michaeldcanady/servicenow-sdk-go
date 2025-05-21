c56f131260359540f1a6abf01a4faec1feb0975d
Michael Canady <michaeldcanady+github@gmail.com>
2025-05-21 01:44:49 +0000

    echo instead of return

--- scripts/test_changed_file.sh
+++ scripts/test_changed_file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 LATEST_RELEASE_TAG=$(gh release list --limit 1 --json tagName --jq '.[0].tagName')
-return $(git diff --name-only $LATEST_RELEASE_TAG HEAD | grep '\.go$' || true)
+echo $(git diff --name-only $LATEST_RELEASE_TAG HEAD | grep '\.go$' || true)
--- scripts/test_version.sh
+++ scripts/test_version.sh
@@ -3,4 +3,4 @@
 CURRENT_VERSION=$(cat VERSION | sed 's/^v//')
 LAST_STABLE_VERSION=$(gh release list --limit 1 --json tagName --jq '.[0].tagName' | sed 's/^v//')
 
-return $CURRENT_VERSION > $LAST_STABLE_VERSION
+echo $($CURRENT_VERSION > $LAST_STABLE_VERSION)
