ea908ff4a0e1a6f11685aa6c367bc61a9e6707ed
Michael Canady <michaeldcanady+github@gmail.com>
2025-05-21 01:57:27 +0000

    invalid format for conditional

--- scripts/test_version.sh
+++ scripts/test_version.sh
@@ -3,7 +3,7 @@
 CURRENT_VERSION=$(cat VERSION | sed 's/^v//')
 LAST_STABLE_VERSION=$(gh release list --limit 1 --json tagName --jq '.[0].tagName' | sed 's/^v//')
 
-if [$CURRENT_VERSION > $LATEST_RELEASE_COMMIT_ID]; then
+if [ $CURRENT_VERSION > $LATEST_RELEASE_COMMIT_ID ]; then
     echo true
 else
     echo false
