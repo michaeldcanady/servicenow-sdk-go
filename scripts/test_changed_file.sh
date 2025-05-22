56150643302683f4ec7ce2ba5900d8ffd27129f1
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2025-05-21 20:32:56 -0400

    Update test_changed_file.sh

--- scripts/test_changed_file.sh
+++ scripts/test_changed_file.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 LATEST_RELEASE_TAG=$(gh release list --limit 1 --json tagName --jq '.[0].tagName')
-echo $(git diff --name-only "${LATEST_RELEASE_TAG}" HEAD | grep '\.go$' || true)
+
+# Get the current state of the repo
+current_state=$(git rev-parse HEAD)
+
+echo $(git diff --name-only $LATEST_RELEASE_TAG $current_state | grep '\.go$' || true)
