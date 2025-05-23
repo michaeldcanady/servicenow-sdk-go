d5fd94cf139f04d6c625230916b05179949eb757
Michael Canady <michaeldcanady+github@gmail.com>
2025-05-23 01:52:42 +0000

    echoing for debugging

--- scripts/test_commit.sh
+++ scripts/test_commit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-git rev-list --tags
+echo "current tags:"
+echo $(git rev-list --tags)
 
 LATEST_RELEASE_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 LATEST_RELEASE_COMMIT_ID=$(git rev-parse "${LATEST_RELEASE_TAG}")
--- scripts/test_version.sh
+++ scripts/test_version.sh
@@ -10,6 +10,9 @@ LAST_STABLE_VERSION_STRING=$(gh release list --limit 1 --json tagName --jq '.[0]
 CURRENT_VERSION=$(version_to_number $CURRENT_VERSION_STRING)
 LAST_STABLE_VERSION=$(version_to_number $LAST_STABLE_VERSION_STRING)
 
+echo "$CURRENT_VERSION"
+echo "$LAST_STABLE_VERSION"
+
 if [ $CURRENT_VERSION -gt $LAST_STABLE_VERSION ]; then
     echo true
 else
