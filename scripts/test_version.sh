6a68af06c216bcbda22419cfa414c5a5b35b629a
Michael Canady <michaeldcanady+github@gmail.com>
2025-05-23 01:48:59 +0000

    adjusting number formatting

--- scripts/test_version.sh
+++ scripts/test_version.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 version_to_number() {
-    echo "$1" | awk -F. '{ printf("%d%03d$03d\n", $1,$2,$3) }'
+    echo "$1" | awk -F. '{ printf("%d%d%d\n", $1,$2,$3) }'
 }
 
 CURRENT_VERSION_STRING=$(cat VERSION | sed 's/^v//')
 LAST_STABLE_VERSION_STRING=$(gh release list --limit 1 --json tagName --jq '.[0].tagName' | sed 's/^v//')
 
-CURRENT_VERSION=$(version_to_number $CURRENT_VERSION_STRING)
-LAST_STABLE_VERSION=$(version_to_number $LAST_STABLE_VERSION_STRING)
+#CURRENT_VERSION=$(version_to_number $CURRENT_VERSION_STRING)
+#LAST_STABLE_VERSION=$(version_to_number $LAST_STABLE_VERSION_STRING)
 
 if [ $CURRENT_VERSION -gt $LAST_STABLE_VERSION ]; then
     echo true
