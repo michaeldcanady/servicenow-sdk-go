85e0c15ec0787f572759192d458fe9fdf40b5439
Michael Canady <michaeldcanady+github@gmail.com>
2025-05-23 01:38:56 +0000

    testing another method for getting the latest release

--- scripts/test_commit.sh
+++ scripts/test_commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LATEST_RELEASE_TAG=$(gh release list --limit 1 --json tagName --jq '.[0].tagName')
+LATEST_RELEASE_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 LATEST_RELEASE_COMMIT_ID=$(git rev-parse "${LATEST_RELEASE_TAG}")
 CURRENT_COMMIT_ID=$(git rev-parse HEAD)
 
