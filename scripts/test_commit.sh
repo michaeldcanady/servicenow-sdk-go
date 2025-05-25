7b393dab3c114f1a9ef536b1cdb60f10ffd0096e
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2025-05-24 22:37:01 -0400

    Update test_commit.sh

--- scripts/test_commit.sh
+++ scripts/test_commit.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "current tags:"
-echo $(git rev-parse HEAD)
-
 LATEST_RELEASE_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 LATEST_RELEASE_COMMIT_ID=$(git rev-parse "${LATEST_RELEASE_TAG}")
 CURRENT_COMMIT_ID=$(git rev-parse HEAD)
