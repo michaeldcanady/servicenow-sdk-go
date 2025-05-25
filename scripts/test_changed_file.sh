76b42dff0402e3901dd56da71bc65e76f6ab1b6f
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2025-05-24 22:40:00 -0400

    Update test_changed_file.sh

--- scripts/test_changed_file.sh
+++ scripts/test_changed_file.sh
@@ -5,4 +5,8 @@ LATEST_RELEASE_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 # Get the current state of the repo
 current_state=$(git rev-parse HEAD)
 
-echo $(git diff --name-only $LATEST_RELEASE_TAG $current_state | grep '\.go$' || true)
+if git diff --name-only "$LATEST_RELEASE_TAG" "$current_state" | grep -q '\.go$'; then
+  echo true
+else
+  echo false
+fi
