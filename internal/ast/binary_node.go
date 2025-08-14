d48fb331352a95c31523532fc74b6a977e6ffcca
Michael Canady <michaeldcanady@gmail.com>
2025-08-14 02:23:33 +0000

    support for binary operations

---
+++ internal/ast/binary_node.go
@@ -0,0 +1,41 @@
+package ast
+
+var _ Node = (*BinaryNode)(nil)
+
+// BinaryNode Represents a binary expression node.
+type BinaryNode struct {
+	// LeftExpression The left-hand side expression of the binary operation.
+	LeftExpression Node
+	// Operator The binary operator.
+	Operator Operator
+	// Position The position of the operator within the source text.
+	Position int
+	// RightExpression The right-hand side expression of the binary operation.
+	RightExpression Node
+}
+
+// Left The leftmost (starting) position of the node in source text.
+func (expr *BinaryNode) Left() int {
+	if expr.LeftExpression == nil {
+		return -1
+	}
+	return expr.LeftExpression.Pos()
+}
+
+// Right The rightmost (ending) position of the node in source text.
+func (expr *BinaryNode) Right() int {
+	if expr.RightExpression == nil {
+		return -1
+	}
+	return expr.RightExpression.Pos()
+}
+
+// Pos The actual position of the node.
+func (expr *BinaryNode) Pos() int {
+	return expr.Position
+}
+
+// Accept Accepts the provided visitor.
+func (expr *BinaryNode) Accept(visitor BinaryNodeVisitor) {
+	visitor.VisitBinaryNode(expr)
+}
---
+++ internal/ast/binary_node_test.go
@@ -0,0 +1,125 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/michaeldcanady/servicenow-sdk-go/internal/mocking"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBinaryNode_Left(t *testing.T) {
+	tests := []struct {
+		name string
+		test func(*testing.T)
+	}{
+		{
+			name: "Has left expression",
+			test: func(t *testing.T) {
+				leftNode := mocking.NewMockNode()
+				leftNode.On("Pos").Return(1)
+
+				node := &BinaryNode{
+					LeftExpression: leftNode,
+				}
+
+				assert.Equal(t, 1, node.Left())
+				leftNode.AssertExpectations(t)
+			},
+		},
+		{
+			name: "No left expression",
+			test: func(t *testing.T) {
+				node := &BinaryNode{
+					LeftExpression: nil,
+				}
+
+				assert.Equal(t, -1, node.Left())
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, test.test)
+	}
+}
+
+func TestBinaryNode_Right(t *testing.T) {
+	tests := []struct {
+		name string
+		test func(*testing.T)
+	}{
+		{
+			name: "Has left expression",
+			test: func(t *testing.T) {
+				rightNode := mocking.NewMockNode()
+				rightNode.On("Pos").Return(1)
+
+				node := &BinaryNode{
+					RightExpression: rightNode,
+				}
+
+				assert.Equal(t, 1, node.Right())
+				rightNode.AssertExpectations(t)
+			},
+		},
+		{
+			name: "No right expression",
+			test: func(t *testing.T) {
+				node := &BinaryNode{
+					RightExpression: nil,
+				}
+
+				assert.Equal(t, -1, node.Right())
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, test.test)
+	}
+}
+
+func TestBinaryNode_Pos(t *testing.T) {
+	tests := []struct {
+		name string
+		test func(*testing.T)
+	}{
+		{
+			name: "Successful",
+			test: func(t *testing.T) {
+				node := &BinaryNode{
+					Position: 1,
+				}
+
+				assert.Equal(t, 1, node.Pos())
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, test.test)
+	}
+}
+
+func TestBinaryNode_Accept(t *testing.T) {
+	tests := []struct {
+		name string
+		test func(*testing.T)
+	}{
+		{
+			name: "Successful",
+			test: func(t *testing.T) {
+				node := &BinaryNode{}
+				visitor := newMockBinaryNodeVisitor()
+				visitor.On("VisitBinaryNode", node)
+
+				node.Accept(visitor)
+				visitor.AssertExpectations(t)
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, test.test)
+	}
+}
