6afdcaeab8e93282f141800c07b56fd7a29f1732
Michael Canady <michaeldcanady@gmail.com>
2025-08-14 02:23:09 +0000

    support for array types

---
+++ internal/ast/array_node.go
@@ -0,0 +1,35 @@
+package ast
+
+var _ Node = (*ArrayNode)(nil)
+
+// ArrayNode represents an array of elements.
+type ArrayNode struct {
+	// Elements elements that make up the array.
+	Elements []Node
+}
+
+// Left The leftmost (starting) position of the node in source text.
+func (expr *ArrayNode) Left() int {
+	return expr.Pos()
+}
+
+// Right The rightmost (ending) position of the node in source text.
+func (expr *ArrayNode) Right() int {
+	if len(expr.Elements) == 0 {
+		return -1
+	}
+	return expr.Elements[len(expr.Elements)-1].Right()
+}
+
+// Pos The actual position of the node.
+func (expr *ArrayNode) Pos() int {
+	if len(expr.Elements) == 0 {
+		return -1
+	}
+	return expr.Elements[0].Left()
+}
+
+// Accept Accepts the provided visitor.
+func (expr *ArrayNode) Accept(visitor ArrayNodeVisitor) {
+	visitor.VisitArrayNode(expr)
+}
