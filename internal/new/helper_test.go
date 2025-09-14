ba0efbad05928febc0eec3a74c2613022e7948bd
michaeldcanady <michaeldcanady+github@gmail.com>
2025-09-14 02:59:07 +0000

    de-dupping

--- internal/new/helper_test.go
+++ internal/new/helper_test.go
@@ -1,7 +1,6 @@
 package internal
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -59,103 +58,6 @@ func TestToPointer(t *testing.T) {
 			test: func(t *testing.T) {
 				val := "string"
 				assert.Equal(t, &val, ToPointer("string"))
-			},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, test.test)
-	}
-}
-
-func TestDereference(t *testing.T) {
-	tests := []struct {
-		name string
-		test func(*testing.T)
-	}{
-		{
-			name: "String pointer",
-			test: func(t *testing.T) {
-				input := ToPointer("testing")
-				output := Dereference(reflect.ValueOf(input))
-
-				assert.Equal(t, interface{}("testing"), output.Interface())
-			},
-		},
-		{
-			name: "String pointer pointer",
-			test: func(t *testing.T) {
-				input := ToPointer(ToPointer("testing"))
-				output := Dereference(reflect.ValueOf(input))
-
-				assert.Equal(t, interface{}("testing"), output.Interface())
-			},
-		},
-		{
-			name: "String",
-			test: func(t *testing.T) {
-				input := "testing"
-				output := Dereference(reflect.ValueOf(input))
-
-				assert.Equal(t, interface{}("testing"), output.Interface())
-			},
-		},
-		{
-			name: "nil",
-			test: func(t *testing.T) {
-				input := (*string)(nil)
-				output := Dereference(reflect.ValueOf(input))
-
-				assert.Equal(t, "", output.Interface())
-			},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, test.test)
-	}
-}
-
-// TODO: tests for all types
-func TestConvert(t *testing.T) {
-	tests := []struct {
-		name string
-		test func(*testing.T)
-	}{
-		{
-			name: "String pointer",
-			test: func(t *testing.T) {
-				var input interface{} = ToPointer("test")
-				var output *string
-
-				err := Convert(input, &output)
-
-				assert.Nil(t, err)
-				assert.Equal(t, ToPointer("test"), output)
-			},
-		},
-		{
-			name: "int8",
-			test: func(t *testing.T) {
-				var input interface{} = int8(8)
-				var output int8
-
-				err := Convert(input, &output)
-
-				assert.Nil(t, err)
-				assert.Equal(t, int8(8), output)
-			},
-		},
-		{
-			name: "String pointer pointer",
-			test: func(t *testing.T) {
-				var input interface{} = ToPointer(ToPointer("test"))
-				var output **string
-
-				err := Convert(input, &output)
-
-				assert.Nil(t, err)
-				assert.Equal(t, ToPointer(ToPointer("test")), output)
 			},
 		},
 	}
