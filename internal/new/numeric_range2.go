2439a7d45bc9b8737148a2e4c3c5b6ef45336786
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2025-09-13 22:55:32 -0400

    Improving type conversion (#275)
    * to find the true value of a pointer
    
    * checking if type is numeric
    
    * convert any int to int
    
    * add support for uint
    
    * no need for duplicate values
    
    * ensure funcs only runs once
    
    * convert value to target type
    
    * new conversion function
    
    * these don't need to be separate
    
    * limiting permissions
    
    * refining permissions

--- internal/new/numeric_range2.go
+++ internal/new/numeric_range2.go
@@ -3,30 +3,38 @@ package internal
 import (
 	"math"
 	"reflect"
+	"sync"
 )
 
 var (
-	int8Range    = newNumericRange2(math.MaxInt8, math.MinInt8, false)
-	int16Range   = newNumericRange2(math.MaxInt16, math.MinInt16, false)
-	int32Range   = newNumericRange2(math.MaxInt32, math.MinInt32, false)
-	int64Range   = newNumericRange2(math.MaxInt64, math.MinInt64, false)
-	float32Range = newNumericRange2(math.MaxFloat32, -math.MaxFloat32, true)
-	float64Range = newNumericRange2(math.MaxFloat64, -math.MaxFloat64, true)
+	int8Range  = sync.OnceValue(func() *numericRange2 { return newNumericRange2(math.MaxInt8, math.MinInt8, false) })()
+	int16Range = sync.OnceValue(func() *numericRange2 { return newNumericRange2(math.MaxInt16, math.MinInt16, false) })()
+	int32Range = sync.OnceValue(func() *numericRange2 { return newNumericRange2(math.MaxInt32, math.MinInt32, false) })()
+	int64Range = sync.OnceValue(func() *numericRange2 { return newNumericRange2(math.MaxInt64, math.MinInt64, false) })()
+	intRange   = sync.OnceValue(func() *numericRange2 { return newNumericRange2(math.MaxInt64, math.MinInt64, true) })()
 
-	uint8Range  = newNumericRange2(math.MaxUint8, 0, false)
-	uint16Range = newNumericRange2(math.MaxUint16, 0, false)
-	uint32Range = newNumericRange2(math.MaxUint32, 0, false)
-	uint64Range = newNumericRange2(math.MaxUint64, 0, false)
+	float32Range = sync.OnceValue(func() *numericRange2 { return newNumericRange2(math.MaxFloat32, -math.MaxFloat32, true) })()
+	float64Range = sync.OnceValue(func() *numericRange2 { return newNumericRange2(math.MaxFloat64, -math.MaxFloat64, true) })()
+
+	uint8Range  = sync.OnceValue(func() *numericRange2 { return newNumericRange2(math.MaxUint8, 0, false) })()
+	uint16Range = sync.OnceValue(func() *numericRange2 { return newNumericRange2(math.MaxUint16, 0, false) })()
+	uint32Range = sync.OnceValue(func() *numericRange2 { return newNumericRange2(math.MaxUint32, 0, false) })()
+	uint64Range = sync.OnceValue(func() *numericRange2 { return newNumericRange2(math.MaxUint64, 0, false) })()
+	uintRange   = sync.OnceValue(func() *numericRange2 { return newNumericRange2(math.MaxUint64, 0, false) })()
 
 	ranges = map[reflect.Kind]*numericRange2{
-		reflect.Int8:    int8Range,
-		reflect.Uint8:   uint8Range,
-		reflect.Int16:   int16Range,
-		reflect.Uint16:  uint16Range,
-		reflect.Int32:   int32Range,
-		reflect.Uint32:  uint32Range,
-		reflect.Int64:   int64Range,
-		reflect.Uint64:  uint64Range,
+		reflect.Int8:  int8Range,
+		reflect.Int16: int16Range,
+		reflect.Int32: int32Range,
+		reflect.Int64: int64Range,
+		reflect.Int:   intRange,
+
+		reflect.Uint8:  uint8Range,
+		reflect.Uint16: uint16Range,
+		reflect.Uint32: uint32Range,
+		reflect.Uint64: uint64Range,
+		reflect.Uint:   uintRange,
+
 		reflect.Float32: float32Range,
 		reflect.Float64: float64Range,
 	}
