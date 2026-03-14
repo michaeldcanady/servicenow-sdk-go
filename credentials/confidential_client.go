22aeaf2465d6e93ba412e6118dce9e0f9aaec0c6
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2026-03-14 18:43:13 -0400

    refactor(credential): use internal oauth2 package (#415)
    Co-authored-by: -g <-g>

--- credentials/confidential_client.go
+++ credentials/confidential_client.go
@@ -2,18 +2,13 @@ package credentials
 
 import (
 	"context"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-	"strings"
+
+	"github.com/michaeldcanady/servicenow-sdk-go/internal/oauth2"
 )
 
 // confidentialClient represents an application that has a client secret.
 type confidentialClient struct {
-	clientID     string
-	clientSecret string
-	tokenRequester
+	oauthClient *oauth2.Client
 }
 
 // newConfidentialClient creates a new confidentialClient.
@@ -33,72 +28,35 @@ func newConfidentialClient(clientID, clientSecret string, authority Authority, o
 		opt(&opts)
 	}
 
-	return &confidentialClient{
-		clientID:     clientID,
-		clientSecret: clientSecret,
-		tokenRequester: &defaultTokenRequester{
-			authority: authority,
-			options:   opts,
+	oauthClient := &oauth2.Client{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoints: &oauth2.Endpoints{
+			TokenURL: authority.TokenURL(),
 		},
+		AuthMethod: oauth2.AuthMethodClientSecretPost, // Default to Post
+		HTTPClient: opts.httpClient,
+	}
+
+	return &confidentialClient{
+		oauthClient: oauthClient,
 	}, nil
 }
 
 // acquireTokenByUsernamePassword acquires a token using the ROPC flow.
 func (c *confidentialClient) acquireTokenByUsernamePassword(ctx context.Context, username, password string) (*AccessToken, error) {
-	params := url.Values{}
-	params.Set("grant_type", "password")
-	params.Set("client_id", c.clientID)
-	params.Set("client_secret", c.clientSecret)
-	params.Set("username", username)
-	params.Set("password", password)
-
-	return c.requestToken(ctx, params)
+	token, err := c.oauthClient.ExchangePassword(ctx, username, password, nil)
+	if err != nil {
+		return nil, err
+	}
+	return convertToken(token), nil
 }
 
 // acquireTokenByRefreshToken acquires a new token using a refresh token.
 func (c *confidentialClient) acquireTokenByRefreshToken(ctx context.Context, refreshToken string) (*AccessToken, error) {
-	params := url.Values{}
-	params.Set("grant_type", "refresh_token")
-	params.Set("client_id", c.clientID)
-	params.Set("client_secret", c.clientSecret)
-	params.Set("refresh_token", refreshToken)
-
-	return c.requestToken(ctx, params)
-}
-
-type defaultTokenRequester struct {
-	authority Authority
-	options   clientOptions
-}
-
-func (c *defaultTokenRequester) requestToken(ctx context.Context, params url.Values) (*AccessToken, error) {
-	tokenURL := c.authority.TokenURL()
-
-	req, err := http.NewRequestWithContext(ctx, "POST", tokenURL, strings.NewReader(params.Encode()))
+	token, err := c.oauthClient.ExchangeRefreshToken(ctx, refreshToken)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create token request: %w", err)
+		return nil, err
 	}
-
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Accept", "application/json")
-
-	// nolint: gosec //G704 external requests can't be processed here
-	resp, err := c.options.httpClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("token request failed: %w", err)
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		body, _ := io.ReadAll(resp.Body) // ignore error here
-		resp.Body.Close()
-		return nil, fmt.Errorf("token request failed with status code %d, body: %s", resp.StatusCode, body)
-	}
-
-	return decodeAccessToken(resp)
-}
-
-func (c *confidentialClient) requestToken(ctx context.Context, params url.Values) (*AccessToken, error) {
-	// Since we know c.tokenRequester is a *defaultTokenRequester (internal knowledge)
-	tr := c.tokenRequester.(*defaultTokenRequester)
-	return tr.requestToken(ctx, params)
+	return convertToken(token), nil
 }
