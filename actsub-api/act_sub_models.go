6a38aac453848350e5f50f04c29672e1a2bc3552
-g <-g>
2026-05-18 12:34:43 +0000

    chore: define data types

--- actsub-api/act_sub_models.go
+++ actsub-api/act_sub_models.go
@@ -1,9 +1,21 @@
 package actsubapi
 
 import (
+	"errors"
+
 	"github.com/michaeldcanady/servicenow-sdk-go/internal"
 	newInternal "github.com/michaeldcanady/servicenow-sdk-go/internal/new"
+	"github.com/michaeldcanady/servicenow-sdk-go/internal/store"
 	"github.com/microsoft/kiota-abstractions-go/serialization"
+	kiotaStore "github.com/microsoft/kiota-abstractions-go/store"
+)
+
+const (
+	messageKey    = "message"
+	streamKey     = "stream"
+	userKey       = "user"
+	activitiesKey = "activities"
+	statusKey     = "status"
 )
 
 // ActivitySubscriptionModel represents a generic model for Activity Subscriptions.
@@ -33,5 +45,202 @@ func (m *ActivitySubscriptionModel) Serialize(writer serialization.Serialization
 
 // GetFieldDeserializers returns the deserialization information for this object.
 func (m *ActivitySubscriptionModel) GetFieldDeserializers() map[string]func(serialization.ParseNode) error {
-	return map[string]func(serialization.ParseNode) error{}
+	return map[string]func(serialization.ParseNode) error{
+		messageKey: func(parseNode serialization.ParseNode) error {
+			val, err := parseNode.GetStringValue()
+			if err != nil {
+				return err
+			}
+			if val != nil {
+				return m.SetMessage(val)
+			}
+			return nil
+		},
+		streamKey: func(parseNode serialization.ParseNode) error {
+			val, err := parseNode.GetStringValue()
+			if err != nil {
+				return err
+			}
+			if val != nil {
+				return m.SetStream(val)
+			}
+			return nil
+		},
+		userKey: func(parseNode serialization.ParseNode) error {
+			val, err := parseNode.GetStringValue()
+			if err != nil {
+				return err
+			}
+			if val != nil {
+				return m.SetUser(val)
+			}
+			return nil
+		},
+		activitiesKey: func(parseNode serialization.ParseNode) error {
+			val, err := parseNode.GetCollectionOfObjectValues(CreateActivityFromDiscriminatorValue)
+			if err != nil {
+				return err
+			}
+			if val != nil {
+				res := make([]*Activity, len(val))
+				for i, v := range val {
+					if v != nil {
+						res[i] = v.(*Activity)
+					}
+				}
+				return m.SetActivities(res)
+			}
+			return nil
+		},
+		statusKey: func(parseNode serialization.ParseNode) error {
+			val, err := parseNode.GetInt64Value()
+			if err != nil {
+				return err
+			}
+			if val != nil {
+				return m.SetStatus(val)
+			}
+			return nil
+		},
+	}
+}
+
+func (m *ActivitySubscriptionModel) GetMessage() (*string, error) {
+	if internal.IsNil(m) {
+		return nil, errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return nil, errors.New("store is nil")
+	}
+
+	val, err := store.DefaultBackedModelAccessorFunc[kiotaStore.BackingStore, *string](backingStore, messageKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return val, nil
+}
+
+func (m *ActivitySubscriptionModel) SetMessage(value *string) error {
+	if internal.IsNil(m) {
+		return errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return errors.New("store is nil")
+	}
+
+	return store.DefaultBackedModelMutatorFunc(backingStore, messageKey, value)
+}
+
+func (m *ActivitySubscriptionModel) GetStream() (*string, error) {
+	if internal.IsNil(m) {
+		return nil, errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return nil, errors.New("store is nil")
+	}
+
+	val, err := store.DefaultBackedModelAccessorFunc[kiotaStore.BackingStore, *string](backingStore, streamKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return val, nil
+}
+
+func (m *ActivitySubscriptionModel) SetStream(value *string) error {
+	if internal.IsNil(m) {
+		return errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return errors.New("store is nil")
+	}
+
+	return store.DefaultBackedModelMutatorFunc(backingStore, streamKey, value)
+}
+
+func (m *ActivitySubscriptionModel) GetUser() (*string, error) {
+	if internal.IsNil(m) {
+		return nil, errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return nil, errors.New("store is nil")
+	}
+
+	return store.DefaultBackedModelAccessorFunc[kiotaStore.BackingStore, *string](backingStore, userKey)
+}
+
+func (m *ActivitySubscriptionModel) SetUser(value *string) error {
+	if internal.IsNil(m) {
+		return errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return errors.New("store is nil")
+	}
+
+	return store.DefaultBackedModelMutatorFunc(backingStore, userKey, value)
+}
+
+func (m *ActivitySubscriptionModel) GetActivities() ([]*Activity, error) {
+	if internal.IsNil(m) {
+		return nil, errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return nil, errors.New("store is nil")
+	}
+
+	return store.DefaultBackedModelAccessorFunc[kiotaStore.BackingStore, []*Activity](backingStore, activitiesKey)
+}
+
+func (m *ActivitySubscriptionModel) SetActivities(value []*Activity) error {
+	if internal.IsNil(m) {
+		return errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return errors.New("store is nil")
+	}
+
+	return store.DefaultBackedModelMutatorFunc(backingStore, activitiesKey, value)
+}
+
+func (m *ActivitySubscriptionModel) GetStatus() (*int64, error) {
+	if internal.IsNil(m) {
+		return nil, errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return nil, errors.New("store is nil")
+	}
+
+	return store.DefaultBackedModelAccessorFunc[kiotaStore.BackingStore, *int64](backingStore, statusKey)
+}
+
+func (m *ActivitySubscriptionModel) SetStatus(value *int64) error {
+	if internal.IsNil(m) {
+		return errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return errors.New("store is nil")
+	}
+
+	return store.DefaultBackedModelMutatorFunc(backingStore, statusKey, value)
 }
---
+++ actsub-api/field.go
@@ -0,0 +1,232 @@
+package actsubapi
+
+import (
+	"errors"
+
+	"github.com/michaeldcanady/servicenow-sdk-go/internal"
+	newInternal "github.com/michaeldcanady/servicenow-sdk-go/internal/new"
+	"github.com/michaeldcanady/servicenow-sdk-go/internal/store"
+	"github.com/microsoft/kiota-abstractions-go/serialization"
+	kiotaStore "github.com/microsoft/kiota-abstractions-go/store"
+)
+
+const (
+	deepLinkToSubObjectKey = "deeplink_to_subobject"
+	displayAsTimeAgoKey    = "display_as_timeago"
+	labelKey               = "label"
+	showLabelKey           = "show_label"
+	typeKey                = "type"
+	valueKey               = "value"
+)
+
+type Field struct {
+	newInternal.BaseModel
+}
+
+func NewField() *Field {
+	return &Field{
+		BaseModel: *newInternal.NewBaseModel(),
+	}
+}
+
+func CreateFieldFromDiscriminatorValue(_ serialization.ParseNode) (serialization.Parsable, error) {
+	return NewField(), nil
+}
+
+func (m *Field) Serialize(writer serialization.SerializationWriter) error {
+	if internal.IsNil(m) {
+		return nil
+	}
+	return nil
+}
+
+func (m *Field) GetFieldDeserializers() map[string]func(serialization.ParseNode) error {
+	return map[string]func(serialization.ParseNode) error{
+		deepLinkToSubObjectKey: func(parseNode serialization.ParseNode) error {
+			val, err := parseNode.GetStringValue()
+			if err != nil {
+				return err
+			}
+			if val != nil {
+				return m.SetDeepLinkToSubObject(val)
+			}
+			return nil
+		},
+		displayAsTimeAgoKey: func(parseNode serialization.ParseNode) error {
+			val, err := parseNode.GetStringValue()
+			if err != nil {
+				return err
+			}
+			if val != nil {
+				return m.SetDisplayAsTimeAgo(val)
+			}
+			return nil
+		},
+		labelKey: func(parseNode serialization.ParseNode) error {
+			val, err := parseNode.GetStringValue()
+			if err != nil {
+				return err
+			}
+			if val != nil {
+				return m.SetLabel(val)
+			}
+			return nil
+		},
+		showLabelKey: func(parseNode serialization.ParseNode) error {
+			val, err := parseNode.GetStringValue()
+			if err != nil {
+				return err
+			}
+			if val != nil {
+				return m.SetShowLabel(val)
+			}
+			return nil
+		},
+		typeKey: func(parseNode serialization.ParseNode) error {
+			val, err := parseNode.GetStringValue()
+			if err != nil {
+				return err
+			}
+			if val != nil {
+				return m.SetType(val)
+			}
+			return nil
+		},
+		valueKey: func(parseNode serialization.ParseNode) error {
+			val, err := parseNode.GetStringValue()
+			if err != nil {
+				return err
+			}
+			if val != nil {
+				return m.SetValue(val)
+			}
+			return nil
+		},
+	}
+}
+
+func (m *Field) GetDeepLinkToSubObject() (*string, error) {
+	if internal.IsNil(m) {
+		return nil, errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return nil, errors.New("store is nil")
+	}
+
+	return store.DefaultBackedModelAccessorFunc[kiotaStore.BackingStore, *string](backingStore, deepLinkToSubObjectKey)
+}
+
+func (m *Field) SetDeepLinkToSubObject(value *string) error {
+	if internal.IsNil(m) {
+		return errors.New("model is nil")
+	}
+
+	return store.DefaultBackedModelMutatorFunc(m.GetBackingStore(), deepLinkToSubObjectKey, value)
+}
+
+func (m *Field) GetDisplayAsTimeAgo() (*string, error) {
+	if internal.IsNil(m) {
+		return nil, errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return nil, errors.New("store is nil")
+	}
+
+	return store.DefaultBackedModelAccessorFunc[kiotaStore.BackingStore, *string](backingStore, displayAsTimeAgoKey)
+}
+
+func (m *Field) SetDisplayAsTimeAgo(value *string) error {
+	if internal.IsNil(m) {
+		return errors.New("model is nil")
+	}
+
+	return store.DefaultBackedModelMutatorFunc(m.GetBackingStore(), displayAsTimeAgoKey, value)
+}
+
+func (m *Field) GetLabel() (*string, error) {
+	if internal.IsNil(m) {
+		return nil, errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return nil, errors.New("store is nil")
+	}
+
+	return store.DefaultBackedModelAccessorFunc[kiotaStore.BackingStore, *string](backingStore, labelKey)
+}
+
+func (m *Field) SetLabel(value *string) error {
+	if internal.IsNil(m) {
+		return errors.New("model is nil")
+	}
+
+	return store.DefaultBackedModelMutatorFunc(m.GetBackingStore(), labelKey, value)
+}
+
+func (m *Field) GetShowLabel() (*string, error) {
+	if internal.IsNil(m) {
+		return nil, errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return nil, errors.New("store is nil")
+	}
+
+	return store.DefaultBackedModelAccessorFunc[kiotaStore.BackingStore, *string](backingStore, showLabelKey)
+}
+
+func (m *Field) SetShowLabel(value *string) error {
+	if internal.IsNil(m) {
+		return errors.New("model is nil")
+	}
+
+	return store.DefaultBackedModelMutatorFunc(m.GetBackingStore(), showLabelKey, value)
+}
+
+func (m *Field) GetType() (*string, error) {
+	if internal.IsNil(m) {
+		return nil, errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return nil, errors.New("store is nil")
+	}
+
+	return store.DefaultBackedModelAccessorFunc[kiotaStore.BackingStore, *string](backingStore, typeKey)
+}
+
+func (m *Field) SetType(value *string) error {
+	if internal.IsNil(m) {
+		return errors.New("model is nil")
+	}
+
+	return store.DefaultBackedModelMutatorFunc(m.GetBackingStore(), typeKey, value)
+}
+
+func (m *Field) GetValue() (*string, error) {
+	if internal.IsNil(m) {
+		return nil, errors.New("model is nil")
+	}
+
+	backingStore := m.GetBackingStore()
+	if internal.IsNil(backingStore) {
+		return nil, errors.New("store is nil")
+	}
+
+	return store.DefaultBackedModelAccessorFunc[kiotaStore.BackingStore, *string](backingStore, valueKey)
+}
+
+func (m *Field) SetValue(value *string) error {
+	if internal.IsNil(m) {
+		return errors.New("model is nil")
+	}
+
+	return store.DefaultBackedModelMutatorFunc(m.GetBackingStore(), valueKey, value)
+}
