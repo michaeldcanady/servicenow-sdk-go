3c5dc5a8645309a49d8e8da35ca1fcf9e1cac820
-g <-g>
2026-05-19 19:09:44 +0000

    fix: lint issues

--- account-api/account_test.go
+++ account-api/account_test.go
@@ -9,12 +9,13 @@ import (
 func TestAccountSerialization(t *testing.T) {
 	account := NewAccount()
 	name := "Boxeo EMEA"
-	account.setName(&name)
+	err := account.setName(&name)
+	assert.NoError(t, err)
 
 	// In a real scenario, we'd use a real SerializationWriter.
 	// Since we are checking if fields are set/retrieved properly,
 	// we can check the backing store directly via getter.
-	
+
 	retrievedName, err := account.GetName()
 	assert.NoError(t, err)
 	assert.Equal(t, &name, retrievedName)
