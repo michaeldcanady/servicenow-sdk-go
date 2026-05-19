14ec37b509cf0b112cf1fd70650223aac57621af
-g <-g>
2026-05-19 19:01:12 +0000

    refactor(account-api): implement backed model pattern for Account

--- account-api/account_response.go
+++ account-api/account_response.go
@@ -6,23 +6,23 @@ import (
 )
 
 // AccountCollectionResponse represents a collection of accounts.
-type AccountCollectionResponse = newInternal.ServiceNowCollectionResponse[*Account]
+type AccountCollectionResponse = newInternal.ServiceNowCollectionResponse[*AccountModel]
 
 // CreateAccountCollectionResponseFromDiscriminatorValue is a factory for creating an AccountCollectionResponse.
 func CreateAccountCollectionResponseFromDiscriminatorValue(_ serialization.ParseNode) (serialization.Parsable, error) {
-	return newInternal.NewBaseServiceNowCollectionResponse[*Account](CreateAccountFromDiscriminatorValue), nil
+	return newInternal.NewBaseServiceNowCollectionResponse[*AccountModel](CreateAccountFromDiscriminatorValue), nil
 }
 
 // AccountCollectionResponseModel is the implementation of AccountCollectionResponse.
-type AccountCollectionResponseModel = newInternal.BaseServiceNowCollectionResponse[*Account]
+type AccountCollectionResponseModel = newInternal.BaseServiceNowCollectionResponse[*AccountModel]
 
 // AccountItemResponse represents a single account response.
-type AccountItemResponse = newInternal.ServiceNowItemResponse[*Account]
+type AccountItemResponse = newInternal.ServiceNowItemResponse[*AccountModel]
 
 // CreateAccountItemResponseFromDiscriminatorValue is a factory for creating an AccountItemResponse.
 func CreateAccountItemResponseFromDiscriminatorValue(_ serialization.ParseNode) (serialization.Parsable, error) {
-	return newInternal.NewBaseServiceNowItemResponse[*Account](CreateAccountFromDiscriminatorValue), nil
+	return newInternal.NewBaseServiceNowItemResponse[*AccountModel](CreateAccountFromDiscriminatorValue), nil
 }
 
 // AccountItemResponseModel is the implementation of AccountItemResponse.
-type AccountItemResponseModel = newInternal.BaseServiceNowItemResponse[*Account]
+type AccountItemResponseModel = newInternal.BaseServiceNowItemResponse[*AccountModel]
---
+++ account-api/account_test.go
@@ -0,0 +1,28 @@
+package accountapi
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAccountSerialization(t *testing.T) {
+	account := NewAccount()
+	name := "Boxeo EMEA"
+	account.setName(&name)
+
+	// In a real scenario, we'd use a real SerializationWriter.
+	// Since we are checking if fields are set/retrieved properly,
+	// we can check the backing store directly via getter.
+	
+	retrievedName, err := account.GetName()
+	assert.NoError(t, err)
+	assert.Equal(t, &name, retrievedName)
+}
+
+func TestCreateAccountFromDiscriminatorValue(t *testing.T) {
+	instance, err := CreateAccountFromDiscriminatorValue(nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, instance)
+	assert.IsType(t, &AccountModel{}, instance)
+}
