8680055a2d12cfa9f1d81015a553e33efd88a794
-g <-g>
2026-06-15 23:00:59 +0000

    fix: remove unused code and imports in credentials package

--- credentials/helper.go
+++ credentials/helper.go
@@ -1,23 +1,10 @@
 package credentials
 
 import (
-	"encoding/json"
-	"net/http"
 	"time"
 
 	"github.com/michaeldcanady/servicenow-sdk-go/internal/oauth2"
 )
-
-func decodeAccessToken(response *http.Response) (*AccessToken, error) {
-	defer response.Body.Close() //nolint:errcheck
-	var accessToken AccessToken
-	if err := json.NewDecoder(response.Body).Decode(&accessToken); err != nil {
-		return nil, err
-	}
-
-	accessToken.ExpiresAt = time.Now().Add(time.Duration(accessToken.ExpiresIn) * time.Second)
-	return &accessToken, nil
-}
 
 func convertToken(t *oauth2.Token) *AccessToken {
 	if t == nil {
--- credentials/mocking.go
+++ credentials/mocking.go
@@ -6,22 +6,6 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-type mockCredential struct {
-	mock.Mock
-}
-
-func newMockCredential() *mockCredential {
-	return &mockCredential{
-		Mock: mock.Mock{},
-	}
-}
-
-func (mock *mockCredential) GetAuthentication() (string, error) {
-	args := mock.Called()
-
-	return args.String(0), args.Error(1)
-}
-
 type mockAuthorizationCodeServer struct {
 	mock.Mock
 }
