4f9026aa1dd860298c6d327f0f258551ac6c553b
-g <-g>
2026-07-08 02:38:00 +0000

    refactor(cdmapplicationsapi): refactor to use refactored accessors

---
+++ cdmapplicationsapi/collection_upload_request.go
@@ -0,0 +1,68 @@
+package cdmapplicationsapi
+
+import (
+	"github.com/michaeldcanady/servicenow-sdk-go/core"
+	"github.com/michaeldcanady/servicenow-sdk-go/internal/conversion"
+	internalSerialization "github.com/michaeldcanady/servicenow-sdk-go/internal/serialization"
+	"github.com/michaeldcanady/servicenow-sdk-go/internal/store"
+	"github.com/microsoft/kiota-abstractions-go/serialization"
+)
+
+// CollectionUploadRequest represents the body for uploading collections.
+type CollectionUploadRequest struct {
+	core.BaseModel
+}
+
+func NewCollectionUploadRequest() *CollectionUploadRequest {
+	return &CollectionUploadRequest{BaseModel: *core.NewBaseModel()}
+}
+
+func (m *CollectionUploadRequest) Serialize(writer serialization.SerializationWriter) error {
+	if conversion.IsNil(m) {
+		return nil
+	}
+	return internalSerialization.Serialize(writer,
+		internalSerialization.SerializeStringFunc(appNameKey)(m.GetAppName),
+		internalSerialization.SerializeStringFunc(collectionNameKey)(m.GetCollectionName),
+		internalSerialization.SerializeStringFunc(dataKey)(m.GetData),
+		internalSerialization.SerializeStringFunc(formatKey)(m.GetFormat),
+	)
+}
+
+func (m *CollectionUploadRequest) GetFieldDeserializers() map[string]func(serialization.ParseNode) error {
+	return map[string]func(serialization.ParseNode) error{
+		appNameKey:        internalSerialization.DeserializeStringFunc()(m.setAppName),
+		collectionNameKey: internalSerialization.DeserializeStringFunc()(m.setCollectionName),
+		dataKey:           internalSerialization.DeserializeStringFunc()(m.setData),
+		formatKey:         internalSerialization.DeserializeStringFunc()(m.setFormat),
+	}
+}
+
+func (m *CollectionUploadRequest) GetAppName() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*CollectionUploadRequest, *string](m, appNameKey)
+}
+func (m *CollectionUploadRequest) setAppName(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, appNameKey, val)
+}
+func (m *CollectionUploadRequest) GetCollectionName() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*CollectionUploadRequest, *string](m, collectionNameKey)
+}
+func (m *CollectionUploadRequest) setCollectionName(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, collectionNameKey, val)
+}
+func (m *CollectionUploadRequest) GetData() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*CollectionUploadRequest, *string](m, dataKey)
+}
+func (m *CollectionUploadRequest) setData(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, dataKey, val)
+}
+func (m *CollectionUploadRequest) GetFormat() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*CollectionUploadRequest, *string](m, formatKey)
+}
+func (m *CollectionUploadRequest) setFormat(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, formatKey, val)
+}
+
+func CreateCollectionUploadRequestFromDiscriminatorValue(_ serialization.ParseNode) (serialization.Parsable, error) {
+	return NewCollectionUploadRequest(), nil
+}
---
+++ cdmapplicationsapi/export_result.go
@@ -0,0 +1,76 @@
+package cdmapplicationsapi
+
+import (
+	"github.com/michaeldcanady/servicenow-sdk-go/core"
+	"github.com/michaeldcanady/servicenow-sdk-go/internal/conversion"
+	internalSerialization "github.com/michaeldcanady/servicenow-sdk-go/internal/serialization"
+	"github.com/michaeldcanady/servicenow-sdk-go/internal/store"
+	"github.com/microsoft/kiota-abstractions-go/serialization"
+)
+
+// ExportResult represents an export result.
+type ExportResult struct {
+	core.BaseModel
+}
+
+func NewExportResult() *ExportResult {
+	return &ExportResult{BaseModel: *core.NewBaseModel()}
+}
+
+func (m *ExportResult) Serialize(writer serialization.SerializationWriter) error {
+	if conversion.IsNil(m) {
+		return nil
+	}
+	return internalSerialization.Serialize(writer,
+		internalSerialization.SerializeStringFunc(sysIdKey)(m.GetSysId),
+		internalSerialization.SerializeStringFunc(nameKey)(m.GetName),
+		internalSerialization.SerializeStringFunc(stateKey)(m.GetState),
+		internalSerialization.SerializeStringFunc(statusKey)(m.GetStatus),
+		internalSerialization.SerializeStringFunc(messageKey)(m.GetMessage),
+	)
+}
+
+func (m *ExportResult) GetFieldDeserializers() map[string]func(serialization.ParseNode) error {
+	return map[string]func(serialization.ParseNode) error{
+		sysIdKey:   internalSerialization.DeserializeStringFunc()(m.setSysId),
+		nameKey:    internalSerialization.DeserializeStringFunc()(m.setName),
+		stateKey:   internalSerialization.DeserializeStringFunc()(m.setState),
+		statusKey:  internalSerialization.DeserializeStringFunc()(m.setStatus),
+		messageKey: internalSerialization.DeserializeStringFunc()(m.setMessage),
+	}
+}
+
+func (m *ExportResult) GetSysId() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*ExportResult, *string](m, sysIdKey)
+}
+func (m *ExportResult) setSysId(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, sysIdKey, val)
+}
+func (m *ExportResult) GetName() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*ExportResult, *string](m, nameKey)
+}
+func (m *ExportResult) setName(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, nameKey, val)
+}
+func (m *ExportResult) GetState() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*ExportResult, *string](m, stateKey)
+}
+func (m *ExportResult) setState(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, stateKey, val)
+}
+func (m *ExportResult) GetStatus() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*ExportResult, *string](m, statusKey)
+}
+func (m *ExportResult) setStatus(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, statusKey, val)
+}
+func (m *ExportResult) GetMessage() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*ExportResult, *string](m, messageKey)
+}
+func (m *ExportResult) setMessage(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, messageKey, val)
+}
+
+func CreateExportResultFromDiscriminatorValue(_ serialization.ParseNode) (serialization.Parsable, error) {
+	return NewExportResult(), nil
+}
