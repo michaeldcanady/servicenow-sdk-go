de8429249351da3384598c4f123fbf92a575c6bd
-g <-g>
2026-07-08 00:20:51 +0000

    refactor(caseapi): refactor to use refactored accessors

---
+++ caseapi/case_result.go
@@ -0,0 +1,173 @@
+package caseapi
+
+import (
+	"github.com/michaeldcanady/servicenow-sdk-go/core"
+	"github.com/michaeldcanady/servicenow-sdk-go/internal/conversion"
+	internalSerialization "github.com/michaeldcanady/servicenow-sdk-go/internal/serialization"
+	"github.com/michaeldcanady/servicenow-sdk-go/internal/store"
+	"github.com/microsoft/kiota-abstractions-go/serialization"
+	kiotaStore "github.com/microsoft/kiota-abstractions-go/store"
+)
+
+// CaseResult represents a single case object.
+type CaseResult interface {
+	serialization.Parsable
+	kiotaStore.BackedModel
+
+	GetSysId() (*string, error)
+	setSysId(*string) error
+	GetNumber() (*string, error)
+	setNumber(*string) error
+	GetShortDescription() (*string, error)
+	setShortDescription(*string) error
+	GetDescription() (*string, error)
+	setDescription(*string) error
+	GetState() (*string, error)
+	setState(*string) error
+	GetPriority() (*string, error)
+	setPriority(*string) error
+	GetCategory() (*string, error)
+	setCategory(*string) error
+	GetAssignmentGroup() (Reference, error)
+	setAssignmentGroup(Reference) error
+	GetAssignedTo() (Reference, error)
+	setAssignedTo(Reference) error
+	GetContact() (Reference, error)
+	setContact(Reference) error
+	GetAccount() (Reference, error)
+	setAccount(Reference) error
+	GetSysCreatedOn() (*string, error)
+	setSysCreatedOn(*string) error
+	GetSysUpdatedOn() (*string, error)
+	setSysUpdatedOn(*string) error
+}
+
+type CaseResultModel struct {
+	core.BaseModel
+}
+
+func NewCaseResult() *CaseResultModel {
+	return &CaseResultModel{BaseModel: *core.NewBaseModel()}
+}
+
+func (m *CaseResultModel) Serialize(writer serialization.SerializationWriter) error {
+	if conversion.IsNil(m) {
+		return nil
+	}
+	return internalSerialization.Serialize(writer,
+		internalSerialization.SerializeStringFunc(sysIdKey)(m.GetSysId),
+		internalSerialization.SerializeStringFunc(numberKey)(m.GetNumber),
+		internalSerialization.SerializeStringFunc(shortDescriptionKey)(m.GetShortDescription),
+		internalSerialization.SerializeStringFunc(descriptionKey)(m.GetDescription),
+		internalSerialization.SerializeStringFunc(stateKey)(m.GetState),
+		internalSerialization.SerializeStringFunc(priorityKey)(m.GetPriority),
+		internalSerialization.SerializeStringFunc(categoryKey)(m.GetCategory),
+		internalSerialization.SerializeObjectValueFunc[Reference](assignmentGroupKey)(m.GetAssignmentGroup),
+		internalSerialization.SerializeObjectValueFunc[Reference](assignedToKey)(m.GetAssignedTo),
+		internalSerialization.SerializeObjectValueFunc[Reference](contactKey)(m.GetContact),
+		internalSerialization.SerializeObjectValueFunc[Reference](accountKey)(m.GetAccount),
+		internalSerialization.SerializeStringFunc(sysCreatedOnKey)(m.GetSysCreatedOn),
+		internalSerialization.SerializeStringFunc(sysUpdatedOnKey)(m.GetSysUpdatedOn),
+	)
+}
+
+func (m *CaseResultModel) GetFieldDeserializers() map[string]func(serialization.ParseNode) error {
+	return map[string]func(serialization.ParseNode) error{
+		sysIdKey:            internalSerialization.DeserializeStringFunc()(m.setSysId),
+		numberKey:           internalSerialization.DeserializeStringFunc()(m.setNumber),
+		shortDescriptionKey: internalSerialization.DeserializeStringFunc()(m.setShortDescription),
+		descriptionKey:      internalSerialization.DeserializeStringFunc()(m.setDescription),
+		stateKey:            internalSerialization.DeserializeStringFunc()(m.setState),
+		priorityKey:         internalSerialization.DeserializeStringFunc()(m.setPriority),
+		categoryKey:         internalSerialization.DeserializeStringFunc()(m.setCategory),
+		assignmentGroupKey:  internalSerialization.DeserializeObjectValueFunc[Reference](CreateReferenceFromDiscriminatorValue)(m.setAssignmentGroup),
+		assignedToKey:       internalSerialization.DeserializeObjectValueFunc[Reference](CreateReferenceFromDiscriminatorValue)(m.setAssignedTo),
+		contactKey:          internalSerialization.DeserializeObjectValueFunc[Reference](CreateReferenceFromDiscriminatorValue)(m.setContact),
+		accountKey:          internalSerialization.DeserializeObjectValueFunc[Reference](CreateReferenceFromDiscriminatorValue)(m.setAccount),
+		sysCreatedOnKey:     internalSerialization.DeserializeStringFunc()(m.setSysCreatedOn),
+		sysUpdatedOnKey:     internalSerialization.DeserializeStringFunc()(m.setSysUpdatedOn),
+	}
+}
+
+func (m *CaseResultModel) GetSysId() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*CaseResultModel, *string](m, sysIdKey)
+}
+func (m *CaseResultModel) setSysId(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, sysIdKey, val)
+}
+func (m *CaseResultModel) GetNumber() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*CaseResultModel, *string](m, numberKey)
+}
+func (m *CaseResultModel) setNumber(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, numberKey, val)
+}
+func (m *CaseResultModel) GetShortDescription() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*CaseResultModel, *string](m, shortDescriptionKey)
+}
+func (m *CaseResultModel) setShortDescription(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, shortDescriptionKey, val)
+}
+func (m *CaseResultModel) GetDescription() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*CaseResultModel, *string](m, descriptionKey)
+}
+func (m *CaseResultModel) setDescription(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, descriptionKey, val)
+}
+func (m *CaseResultModel) GetState() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*CaseResultModel, *string](m, stateKey)
+}
+func (m *CaseResultModel) setState(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, stateKey, val)
+}
+func (m *CaseResultModel) GetPriority() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*CaseResultModel, *string](m, priorityKey)
+}
+func (m *CaseResultModel) setPriority(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, priorityKey, val)
+}
+func (m *CaseResultModel) GetCategory() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*CaseResultModel, *string](m, categoryKey)
+}
+func (m *CaseResultModel) setCategory(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, categoryKey, val)
+}
+func (m *CaseResultModel) GetAssignmentGroup() (Reference, error) {
+	return store.DefaultBackedModelAccessorFunc[*CaseResultModel, Reference](m, assignmentGroupKey)
+}
+func (m *CaseResultModel) setAssignmentGroup(val Reference) error {
+	return store.DefaultBackedModelMutatorFunc(m, assignmentGroupKey, val)
+}
+func (m *CaseResultModel) GetAssignedTo() (Reference, error) {
+	return store.DefaultBackedModelAccessorFunc[*CaseResultModel, Reference](m, assignedToKey)
+}
+func (m *CaseResultModel) setAssignedTo(val Reference) error {
+	return store.DefaultBackedModelMutatorFunc(m, assignedToKey, val)
+}
+func (m *CaseResultModel) GetContact() (Reference, error) {
+	return store.DefaultBackedModelAccessorFunc[*CaseResultModel, Reference](m, contactKey)
+}
+func (m *CaseResultModel) setContact(val Reference) error {
+	return store.DefaultBackedModelMutatorFunc(m, contactKey, val)
+}
+func (m *CaseResultModel) GetAccount() (Reference, error) {
+	return store.DefaultBackedModelAccessorFunc[*CaseResultModel, Reference](m, accountKey)
+}
+func (m *CaseResultModel) setAccount(val Reference) error {
+	return store.DefaultBackedModelMutatorFunc(m, accountKey, val)
+}
+func (m *CaseResultModel) GetSysCreatedOn() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*CaseResultModel, *string](m, sysCreatedOnKey)
+}
+func (m *CaseResultModel) setSysCreatedOn(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, sysCreatedOnKey, val)
+}
+func (m *CaseResultModel) GetSysUpdatedOn() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*CaseResultModel, *string](m, sysUpdatedOnKey)
+}
+func (m *CaseResultModel) setSysUpdatedOn(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, sysUpdatedOnKey, val)
+}
+
+func CreateCaseResultFromDiscriminatorValue(_ serialization.ParseNode) (serialization.Parsable, error) {
+	return NewCaseResult(), nil
+}
---
+++ caseapi/field_values_result.go
@@ -0,0 +1,83 @@
+package caseapi
+
+import (
+	"github.com/michaeldcanady/servicenow-sdk-go/core"
+	"github.com/michaeldcanady/servicenow-sdk-go/internal/conversion"
+	internalSerialization "github.com/michaeldcanady/servicenow-sdk-go/internal/serialization"
+	"github.com/michaeldcanady/servicenow-sdk-go/internal/store"
+	"github.com/microsoft/kiota-abstractions-go/serialization"
+	kiotaStore "github.com/microsoft/kiota-abstractions-go/store"
+)
+
+// FieldValuesResult represents field values.
+type FieldValuesResult interface {
+	serialization.Parsable
+	kiotaStore.BackedModel
+
+	GetLabel() (*string, error)
+	setLabel(*string) error
+	GetValue() (*string, error)
+	setValue(*string) error
+	GetSequence() (*int32, error)
+	setSequence(*int32) error
+	GetDependentValue() (*string, error)
+	setDependentValue(*string) error
+}
+
+type FieldValuesResultModel struct {
+	core.BaseModel
+}
+
+func NewFieldValuesResult() *FieldValuesResultModel {
+	return &FieldValuesResultModel{BaseModel: *core.NewBaseModel()}
+}
+
+func (m *FieldValuesResultModel) Serialize(writer serialization.SerializationWriter) error {
+	if conversion.IsNil(m) {
+		return nil
+	}
+	return internalSerialization.Serialize(writer,
+		internalSerialization.SerializeStringFunc(labelKey)(m.GetLabel),
+		internalSerialization.SerializeStringFunc(valueKey)(m.GetValue),
+		internalSerialization.SerializeInt32Func(sequenceKey)(m.GetSequence),
+		internalSerialization.SerializeStringFunc(dependentValueKey)(m.GetDependentValue),
+	)
+}
+
+func (m *FieldValuesResultModel) GetFieldDeserializers() map[string]func(serialization.ParseNode) error {
+	return map[string]func(serialization.ParseNode) error{
+		labelKey:          internalSerialization.DeserializeStringFunc()(m.setLabel),
+		valueKey:          internalSerialization.DeserializeStringFunc()(m.setValue),
+		sequenceKey:       internalSerialization.DeserializeInt32Func()(m.setSequence),
+		dependentValueKey: internalSerialization.DeserializeStringFunc()(m.setDependentValue),
+	}
+}
+
+func (m *FieldValuesResultModel) GetLabel() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*FieldValuesResultModel, *string](m, labelKey)
+}
+func (m *FieldValuesResultModel) setLabel(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, labelKey, val)
+}
+func (m *FieldValuesResultModel) GetValue() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*FieldValuesResultModel, *string](m, valueKey)
+}
+func (m *FieldValuesResultModel) setValue(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, valueKey, val)
+}
+func (m *FieldValuesResultModel) GetSequence() (*int32, error) {
+	return store.DefaultBackedModelAccessorFunc[*FieldValuesResultModel, *int32](m, sequenceKey)
+}
+func (m *FieldValuesResultModel) setSequence(val *int32) error {
+	return store.DefaultBackedModelMutatorFunc(m, sequenceKey, val)
+}
+func (m *FieldValuesResultModel) GetDependentValue() (*string, error) {
+	return store.DefaultBackedModelAccessorFunc[*FieldValuesResultModel, *string](m, dependentValueKey)
+}
+func (m *FieldValuesResultModel) setDependentValue(val *string) error {
+	return store.DefaultBackedModelMutatorFunc(m, dependentValueKey, val)
+}
+
+func CreateFieldValuesResultFromDiscriminatorValue(_ serialization.ParseNode) (serialization.Parsable, error) {
+	return NewFieldValuesResult(), nil
+}
